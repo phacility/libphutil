98539c8c42a96ba1256e9bee1de89f0f5d9de1d4
jungejason <jun.ge.jason@fb.com>
2011-05-26 14:20:30 -0700

    Add logging for XHPAST syntax highlighter
    Summary:
    log the exception when XHPAST fails to parse.
    
    Test Plan:
    verified that the exception is logged when XHPAST throws an
    exception.
    
    Reviewed By: tuomaspelkonen
    Reviewers: tuomaspelkonen, epriestley
    CC: aran, tuomaspelkonen
    Differential Revision: 351

--- src/markup/syntax/highlighter/xhpast/PhutilXHPASTSyntaxHighlighter.php
+++ src/markup/syntax/highlighter/xhpast/PhutilXHPASTSyntaxHighlighter.php
@@ -26,92 +26,97 @@ class PhutilXHPASTSyntaxHighlighter {
   }
 
   public function highlightSource($text) {
-    $scrub = false;
-    if (strpos($text, '<?') === false) {
-      $text = "<?php\n".$text."\n";
-      $scrub = true;
-    }
+    try {
+      $scrub = false;
+      if (strpos($text, '<?') === false) {
+        $text = "<?php\n".$text."\n";
+        $scrub = true;
+      }
 
-    $tree = XHPASTTree::newFromData($text);
+      $tree = XHPASTTree::newFromData($text);
 
-    $out = array();
-    $next = null;
-    foreach ($tree->getRootNode()->getTokens() as $token) {
-      $value = phutil_escape_html($token->getValue());
-      $class = null;
-      $multi = false;
-      switch ($token->getTypeName()) {
-        case 'T_WHITESPACE':
-          break;
-        case 'T_DOC_COMMENT':
-          $class = 'dc';
-          $multi = true;
-          break;
-        case 'T_COMMENT':
-          $class = 'c';
-          $multi = true;
-          break;
-        case 'T_CONSTANT_ENCAPSED_STRING':
-        case 'T_ENCAPSED_AND_WHITESPACE':
-        case 'T_INLINE_HTML':
-          $class = 's';
-          $multi = true;
-          break;
-        case 'T_VARIABLE':
-          $class = 'nv';
-          break;
-        case 'T_OPEN_TAG':
-        case 'T_OPEN_TAG_WITH_ECHO':
-        case 'T_CLOSE_TAG':
-          $class = 'o';
-          break;
-        case 'T_OBJECT_OPERATOR':
-          $next = 'na';
-          $class = 'k';
-          break;
-        case 'T_LNUMBER':
-        case 'T_DNUMBER':
-          $class = 'm';
-          break;
-        case 'T_STRING':
-          static $magic = array(
-            'true' => true,
-            'false' => true,
-            'null' => true,
-          );
-          if (isset($magic[$value])) {
+      $out = array();
+      $next = null;
+      foreach ($tree->getRootNode()->getTokens() as $token) {
+        $value = phutil_escape_html($token->getValue());
+        $class = null;
+        $multi = false;
+        switch ($token->getTypeName()) {
+          case 'T_WHITESPACE':
+            break;
+          case 'T_DOC_COMMENT':
+            $class = 'dc';
+            $multi = true;
+            break;
+          case 'T_COMMENT':
+            $class = 'c';
+            $multi = true;
+            break;
+          case 'T_CONSTANT_ENCAPSED_STRING':
+          case 'T_ENCAPSED_AND_WHITESPACE':
+          case 'T_INLINE_HTML':
+            $class = 's';
+            $multi = true;
+            break;
+          case 'T_VARIABLE':
+            $class = 'nv';
+            break;
+          case 'T_OPEN_TAG':
+          case 'T_OPEN_TAG_WITH_ECHO':
+          case 'T_CLOSE_TAG':
+            $class = 'o';
+            break;
+          case 'T_OBJECT_OPERATOR':
+            $next = 'na';
             $class = 'k';
             break;
-          }
-          if ($next) {
-            $class = $next;
-            $next = null;
-          } else {
-            $class = 'nx';
-          }
-          break;
-        default:
-          $class = 'k';
-          break;
-      }
-      if ($class) {
-        $value = '<span class="'.$class.'">'.$value.'</span>';
-        if ($multi) {
-          $value = str_replace(
-            "\n",
-            "</span>\n<span=\"{$class}\">",
-            $value);
+          case 'T_LNUMBER':
+          case 'T_DNUMBER':
+            $class = 'm';
+            break;
+          case 'T_STRING':
+            static $magic = array(
+              'true' => true,
+              'false' => true,
+              'null' => true,
+            );
+            if (isset($magic[$value])) {
+              $class = 'k';
+              break;
+            }
+            if ($next) {
+              $class = $next;
+              $next = null;
+            } else {
+              $class = 'nx';
+            }
+            break;
+          default:
+            $class = 'k';
+            break;
         }
-        $out[] = $value;
-      } else {
-        $out[] = $value;
+        if ($class) {
+          $value = '<span class="'.$class.'">'.$value.'</span>';
+          if ($multi) {
+            $value = str_replace(
+              "\n",
+              "</span>\n<span=\"{$class}\">",
+              $value);
+          }
+          $out[] = $value;
+        } else {
+          $out[] = $value;
+        }
       }
-    }
 
-    if ($scrub) {
-      array_shift($out);
-    }
+      if ($scrub) {
+        array_shift($out);
+      }
 
-    return rtrim(implode('', $out));
+      return rtrim(implode('', $out));
+    } catch (Exception $ex) {
+      phlog($ex);
+      throw $ex;
+    }
   }
 }
