5556fd5cc6ad5b7fad7738766e4ce05facb05c51
epriestley <git@epriestley.com>
2012-01-11 14:05:10 -0800

    Add a blanket guard for 'javascript:' hrefs in libphutil
    Summary:
    In our application (and, I claim in the comments, any "well designed"
    application) these are never intentional and always indicate an attack vector.
    D1365 closes such a vector (albeit an admin-only one), but we can also just put
    a blanket check here.
    
    On its own this would be a poor approach because it's a blacklist rather than a
    whitelist (it does nothign about these URIs in other attributes, or about other
    dangerous URIs), but I think it's a reasonable compliment to other mechanisms
    and practices, like the SQL syntax error checks.
    
    Test Plan:
      - Ran unit tests.
      - Browsed site.
      - Profiled some heavy pages, this adds less than a millisecond to a 1000-task
    Maniphest list.
    
    Reviewers: btrahan, jungejason, arice
    
    Reviewed By: arice
    
    CC: aran, arice, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1366

---
+++ src/markup/__tests__/PhutilMarkupTestCase.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * Copyright 2012 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @group testcase
+ */
+class PhutilMarkupTestCase extends ArcanistPhutilTestCase {
+
+  public function testTagDefaults() {
+    $this->assertEqual(
+      phutil_render_tag('x'),
+      phutil_render_tag('x', array()));
+
+    $this->assertEqual(
+      phutil_render_tag('x', array()),
+      phutil_render_tag('x', array(), null));
+  }
+
+  public function testTagEmpty() {
+    $this->assertEqual(
+      '<x />',
+      phutil_render_tag('x', array(), null));
+
+    $this->assertEqual(
+      '<x></x>',
+      phutil_render_tag('x', array(), ''));
+  }
+
+
+  public function testTagBasics() {
+    $this->assertEqual(
+      '<x />',
+      phutil_render_tag('x'));
+
+    $this->assertEqual(
+      '<x>y</x>',
+      phutil_render_tag('x', array(), 'y'));
+  }
+
+  public function testTagAttributes() {
+    $this->assertEqual(
+      '<x u="v">y</x>',
+      phutil_render_tag('x', array('u' => 'v'), 'y'));
+
+    $this->assertEqual(
+      '<x u="v" />',
+      phutil_render_tag('x', array('u' => 'v')));
+  }
+
+  public function testTagEscapes() {
+    $this->assertEqual(
+      '<x u="&lt;" />',
+      phutil_render_tag('x', array('u' => '<')));
+
+    $this->assertEqual(
+      '<x><y /></x>',
+      phutil_render_tag('x', array(), phutil_render_tag('y')));
+  }
+
+  public function testTagJavascriptProtocolRejection() {
+    $hrefs = array(
+      'javascript:alert(1)'         => true,
+      'JAVASCRIPT:alert(1)'         => true,
+      '     javascript:alert(1)'    => true,
+      '/'                           => false,
+      '/path/to/stuff/'             => false,
+      ''                            => false,
+      'http://example.com/'         => false,
+      '#'                           => false,
+    );
+
+    foreach (array(true, false) as $use_uri) {
+      foreach ($hrefs as $href => $expect) {
+        if ($use_uri) {
+          $href = new PhutilURI($href);
+        }
+
+        $caught = null;
+        try {
+          phutil_render_tag('a', array('href' => $href), 'click for candy');
+        } catch (Exception $ex) {
+          $caught = $ex;
+        }
+        $this->assertEqual(
+          $expect,
+          $caught instanceof Exception,
+          "Rejected href: {$href}");
+      }
+    }
+  }
+
+}
--- src/markup/render.php
+++ src/markup/render.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,27 @@
  * @group markup
  */
 function phutil_render_tag($tag, array $attributes = array(), $content = null) {
+
+  if (!empty($attributes['href'])) {
+
+    // This might be a URI object, so cast it to a string.
+    $href = (string)$attributes['href'];
+
+    // Block 'javascript:' hrefs at the tag level: no well-designed application
+    // should ever use them, and they are a potent attack vector. This function
+    // is deep in the core and performance sensitive, so skip the relatively
+    // expensive preg_match() call if the initial character is '/' (this is the
+    // case with essentially every URI Phabricator renders).
+    if (isset($href[0]) &&
+        ($href[0] != '/') &&
+        preg_match('/^\s*javascript:/i', $href)) {
+      throw new Exception(
+        "Attempting to render a tag with an 'href' attribute that begins ".
+        "with 'javascript:'. This is either a serious security concern or a ".
+        "serious architecture concern. Seek urgent remedy.");
+    }
+  }
+
   foreach ($attributes as $k => $v) {
     if ($v === null) {
       continue;
