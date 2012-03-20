efb6a7d305c0455f53d878bb721e09892e261924
epriestley <git@epriestley.com>
2012-03-19 19:12:21 -0700

    Provide a mechanism for reverisbly decoding arbitrary data for infix URIs
    Summary:
    See next diff. In Diffusion, we want to put arbitrary data into the URI and keep it as readable as possible.
    
    Correct phutil_escape_uri()'s handling of slashes for suffix or query parameter use.
    
    Add phutil_escape_uri_path_component() to correctly handle arbitary URI infixes (notably, branch names).
    
    Test Plan: See next diff.
    
    Reviewers: nh, vrana, btrahan
    
    Reviewed By: btrahan
    
    CC: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1920

--- src/markup/__tests__/PhutilMarkupTestCase.php
+++ src/markup/__tests__/PhutilMarkupTestCase.php
@@ -72,6 +72,12 @@ final class PhutilMarkupTestCase extends ArcanistPhutilTestCase {
       phutil_render_tag('x', array(), phutil_render_tag('y')));
   }
 
+  public function testTagNullAttribute() {
+    $this->assertEqual(
+      '<x />',
+      phutil_render_tag('x', array('y' => null)));
+  }
+
   public function testTagJavascriptProtocolRejection() {
     $hrefs = array(
       'javascript:alert(1)'         => true,
@@ -102,6 +108,35 @@ final class PhutilMarkupTestCase extends ArcanistPhutilTestCase {
           "Rejected href: {$href}");
       }
     }
+  }
+
+  public function testURIEscape() {
+    $this->assertEqual(
+      '%2B/%20%3F%23%26%3A%21xyz%25',
+      phutil_escape_uri('+/ ?#&:!xyz%'));
+  }
+
+  public function testURIPathComponentEscape() {
+    $this->assertEqual(
+      'a%252Fb',
+      phutil_escape_uri_path_component('a/b'));
+
+    $str = '';
+    for ($ii = 0; $ii <= 255; $ii++) {
+      $str .= chr($ii);
+    }
+
+    $this->assertEqual(
+      $str,
+      phutil_unescape_uri_path_component(
+        rawurldecode( // Simulates webserver.
+          phutil_escape_uri_path_component($str))));
+  }
+
+  public function testHsprintf() {
+    $this->assertEqual(
+      '<div>&lt;3</div>',
+      hsprintf('<div>%s</div>', '<3'));
   }
 
 }
--- src/markup/render.php
+++ src/markup/render.php
@@ -77,9 +77,76 @@ function hsprintf($html/*, ... */) {
   return vsprintf($html, array_map('phutil_escape_html', $args));
 }
 
+
 /**
+ * Escape text for inclusion in a URI or a query parameter. Note that this
+ * method does NOT escape '/', because "%2F" is invalid in paths and Apache
+ * will automatically 404 the page if it's present. This will produce correct
+ * (the URIs will work) and desirable (the URIs will be readable) behavior in
+ * these cases:
+ *
+ *    '/path/?param='.phutil_escape_uri($string);         # OK: Query Parameter
+ *    '/path/to/'.phutil_escape_uri($string);             # OK: URI Suffix
+ *
+ * It will potentially produce the WRONG behavior in this special case:
+ *
+ *    COUNTEREXAMPLE
+ *    '/path/to/'.phutil_escape_uri($string).'/thing/';   # BAD: URI Infix
+ *
+ * In this case, any '/' characters in the string will not be escaped, so you
+ * will not be able to distinguish between the string and the suffix (unless
+ * you have more information, like you know the format of the suffix). For infix
+ * URI components, use @{function:phutil_escape_uri_path_component} instead.
+ *
+ * @param   string  Some string.
+ * @return  string  URI encoded string, except for '/'.
+ *
  * @group markup
  */
 function phutil_escape_uri($string) {
-  return rawurlencode($string);
+  return str_replace('%2F', '/', rawurlencode($string));
+}
+
+
+/**
+ * Escape text for inclusion as an infix URI substring. See discussion at
+ * @{function:phutil_escape_uri}. This function covers an unusual special case;
+ * @{function:phutil_escape_uri} is usually the correct function to use.
+ *
+ * This function will escape a string into a format which is safe to put into
+ * a URI path and which does not contain '/' so it can be correctly parsed when
+ * embedded as a URI infix component.
+ *
+ * However, you MUST decode the string with
+ * @{function:phutil_decode_uri_path_component} before it can be used in the
+ * application.
+ *
+ * @param   string  Some string.
+ * @return  string  URI encoded string that is safe for infix composition.
+ *
+ * @group markup
+ */
+function phutil_escape_uri_path_component($string) {
+  return rawurlencode(rawurlencode($string));
+}
+
+
+/**
+ * Unescape text that was escaped by
+ * @{function:phutil_escape_uri_path_component}. See
+ * @{function:phutil_escape_uri} for discussion.
+ *
+ * Note that this function is NOT the inverse of
+ * @{function:phutil_escape_uri_path_component}! It undoes additional escaping
+ * which is added to survive the implied unescaping performed by the webserver
+ * when interpreting the request.
+ *
+ * @param string  Some string emitted
+ *                from @{function:phutil_escape_uri_path_component} and
+ *                then accessed via a web server.
+ * @return string Original string.
+ * @group markup
+ */
+function phutil_unescape_uri_path_component($string) {
+  return rawurldecode($string);
 }
