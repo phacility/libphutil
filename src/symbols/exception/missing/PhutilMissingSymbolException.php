58e07c5ca82f29d21fd69094f394176fe646f8be
epriestley <git@epriestley.com>
2012-04-06 14:09:44 -0700

    Fix PhutilMissingSymbolException documentation pointer
    Summary: See rPHU91e4f9144e944c2653ce85174abc26099afa61d1. The controlling documentation moved to Phabricator after 'arc liberate', tailor the instructions for modern toolsets / doc locations.
    
    Test Plan: Added "new Derp()" to a script, ran it, got a more useful/actionable error.
    
    Reviewers: vrana, btrahan
    
    Reviewed By: vrana
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D2129

--- src/symbols/exception/missing/PhutilMissingSymbolException.php
+++ src/symbols/exception/missing/PhutilMissingSymbolException.php
@@ -23,8 +23,10 @@ final class PhutilMissingSymbolException extends Exception {
   public function __construct($symbol) {
     parent::__construct(
       "Failed to load symbol '{$symbol}'. ".
-      "Your library map may be out of date. Try to rebuild it with ".
-      "instructions at page http://www.phabricator.com/docs/libphutil/article/".
-      "Rebuilding_the_Library_Map.html.");
+      "If this symbol was recently added or moved, your library map may ".
+      "be out of date. You can rebuild the map by running 'arc liberate'. ".
+      "For more information, see: ".
+      "http://www.phabricator.com/docs/phabricator/article/".
+      "libphutil_Libraries_User_Guide.html");
   }
 }
