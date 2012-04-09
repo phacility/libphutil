phacility/libphutil

commit 472b02365c3f45d51ca4190a1cdff31cc90ac0e8

Allow [[]] remarkup links outside Phabricator
Summary: Namely at [[http://phabricator.com/docs/phabricator/article/Give_Feedback!_Get_Support!.html | /docs/article/Give_Feedback!_Get_Support!.html]].

Test Plan:
  [[wiki]]
  [[http://example.com]]
  [[http://example.com | example.com]]

Regenerate docs.

Reviewers: epriestley

Reviewed By: epriestley

CC: aran

Differential Revision: https://secure.phabricator.com/D2110

src/markup/engine/remarkup/markuprule/hyperlink/PhutilRemarkupRuleHyperlink.php
@@ -51,6 +51,11 @@ class PhutilRemarkupRuleHyperlink
       array($this, 'markupHyperlinkUngreedy'),
       $text);
 
+    $text = preg_replace_callback(
+      '@\B\\[\\[([^|\\]]+)(?:\\|([^\\]]+))?\\]\\]\B@U',
+      array($this, 'markupDocumentLink'),
+      $text);
+
     return $text;
   }
 
@@ -74,14 +79,14 @@ class PhutilRemarkupRuleHyperlink
     return $this->getEngine()->storeText($this->renderHyperlink($link));
   }
 
-  protected function renderHyperlink($link) {
+  protected function renderHyperlink($link, $name = null) {
     return phutil_render_tag(
       'a',
       array(
         'href'    => $link,
         'target'  => '_blank',
       ),
-      phutil_escape_html($link));
+      phutil_escape_html($name === null ? $link : $name));
   }
 
   private function markupHyperlinkUngreedy($matches) {
@@ -109,6 +114,32 @@ class PhutilRemarkupRuleHyperlink
     }
 
     return $this->markupHyperlink(array(null, $match)).$tail;
+  }
+
+  public function markupDocumentLink($matches) {
+    $uri = trim($matches[1]);
+    $name = trim(idx($matches, 2, $uri));
+
+    // If whatever is being linked to begins with "/" or has "://", treat it
+    // as a URI instead of a wiki page.
+    $is_uri = preg_match('@(^/)|(://)@', $uri);
+
+    if ($is_uri) {
+      $protocols = $this->getEngine()->getConfig(
+        'uri.allowed-protocols',
+        array());
+      $protocol = id(new PhutilURI($uri))->getProtocol();
+      if (!idx($protocols, $protocol)) {
+        // Don't treat this as a URI if it's not an allowed protocol.
+        $is_uri = false;
+      }
+    }
+
+    if (!$is_uri) {
+      return $matches[0];
+    }
+
+    return $this->getEngine()->storeText($this->renderHyperlink($uri, $name));
   }
 
 }

commit 7da5c6164daf40cf57446fe36ea727bf2e03e15c

Fix external [[ ]] Remarkup links
Summary: Broken by D2110 and D2111.

Test Plan:
Disable `differential.custom-remarkup-rules` which tricked my original test plan.

  [[ http://example.com | test ]]

Reviewers: Koolvin, epriestley

Reviewed By: epriestley

CC: aran, epriestley

Maniphest Tasks: T1090

Differential Revision: https://secure.phabricator.com/D2164

src/markup/engine/remarkup/markuprule/hyperlink/PhutilRemarkupRuleHyperlink.php
@@ -25,6 +25,11 @@ class PhutilRemarkupRuleHyperlink
 
   public function apply($text) {
 
+    $text = preg_replace_callback(
+      '@\B\\[\\[([^|\\]]+)(?:\\|([^\\]]+))?\\]\\]\B@U',
+      array($this, 'markupDocumentLink'),
+      $text);
+
     // Hyperlinks with explicit "<>" around them get linked exactly, without
     // the "<>". Angle brackets are basically special and mean "this is a URL
     // with weird characters". This is assumed to be reasonable because they
@@ -49,11 +54,6 @@ class PhutilRemarkupRuleHyperlink
     $text = preg_replace_callback(
       '@(?<=^|\s)(\w{3,}://\S+)(?=\s|$)@',
       array($this, 'markupHyperlinkUngreedy'),
-      $text);
-
-    $text = preg_replace_callback(
-      '@\B\\[\\[([^|\\]]+)(?:\\|([^\\]]+))?\\]\\]\B@U',
-      array($this, 'markupDocumentLink'),
       $text);
 
     return $text;