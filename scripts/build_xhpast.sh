b983e82db5f3515ee97cd20f3092200d387d60e3
Jason Ge <jungejason@fb.com>
2012-04-11 20:21:31 -0700

    Fix error message in build_xhpast
    Summary:
    escape backtick
    
    it was displayed as
    
      ...
      Maybe try  if the error
      ...
    
    Test Plan: run build_xhpast and see 'Static=1...' got displayed.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D2211

--- scripts/build_xhpast.sh
+++ scripts/build_xhpast.sh
@@ -18,7 +18,7 @@ if ! ./xhpast --version
 then
   echo
   echo >&2 "xhpast is broken :("
-  echo >&2 "Maybe try `STATIC=1 ./build_xhpast.sh` if the error"
+  echo >&2 "Maybe try \`STATIC=1 ./build_xhpast.sh\` if the error"
   echo >&2 "is linker-related?"
   echo
   exit 2
