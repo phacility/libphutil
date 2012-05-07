0cc4b2cf2470e40ec4f79197d67dcb8914d8c7fa
epriestley <git@epriestley.com>
2012-05-07 11:18:44 -0700

    Disable `xdebug.max_nesting_level` in __init_script__.php
    Summary:
      - See T1192.
      - Do all the environment stuff //before// including code.
      - Do output buffer stuff first since it will completely silence any errors in the other things.
    
    Test Plan:
      - Set `xdebug.max_nesting_level` to 5, ran "arc". After this change, no error.
    
    Reviewers: btrahan, jungejason
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T1192
    
    Differential Revision: https://secure.phabricator.com/D2414

--- scripts/__init_script__.php
+++ scripts/__init_script__.php
@@ -17,17 +17,40 @@
  */
 
 function __phutil_init_script__() {
-  $root = dirname(dirname(__FILE__));
-  require_once $root.'/src/__phutil_library_init__.php';
-
   // Adjust the runtime language configuration to be reasonable and inline with
-  // expectations.
+  // expectations. We do this first, then load libraries.
+
+  // There may be some kind of auto-prepend script configured which starts an
+  // output buffer. Discard any such output buffers so messages can be sent to
+  // stdout (if a user wants to capture output from a script, there are a large
+  // number of ways they can accomplish it legitimately; historically, we ran
+  // into this on only one install which had some bizarre configuration, but it
+  // was difficult to diagnose because the symptom is "no messages of any
+  // kind").
+  while (ob_get_level() > 0) {
+    ob_end_clean();
+  }
 
   error_reporting(E_ALL | E_STRICT);
 
   $config_map = array(
-    'display_errors'  => true,
-    'error_log'       => null,
+    // Always display script errors. Without this, they may not appear, which is
+    // unhelpful when users encounter a problem. On the web this is a security
+    // concern because you don't want to expose errors to clients, but in a
+    // script context we always want to show errors.
+    'display_errors'              => true,
+
+    // Set the error log to the default, so errors go to stderr. Without this
+    // errors may end up in some log, and users may not know where the log is
+    // or check it.
+    'error_log'                   => null,
+
+    // XDebug raises a fatal error if the call stack gets too deep, but the
+    // default setting is 100, which we may exceed legitimately with module
+    // includes (and in other cases, like recursive filesystem operations
+    // applied to 100+ levels of directory nesting). Stop it from triggering:
+    // we explicitly limit recursive algorithms which should be limited.
+    'xdebug.max_nesting_level'    => null,
   );
 
   foreach ($config_map as $config_key => $config_value) {
@@ -45,16 +68,10 @@ function __phutil_init_script__() {
     date_default_timezone_set('UTC');
   }
 
-  // There may be some kind of auto-prepend script configured which starts an
-  // output buffer. Discard any such output buffers so messages can be sent to
-  // stdout (if a user wants to capture output from a script, there are a large
-  // number of ways they can accomplish it legitimately; historically, we ran
-  // into this on only one install which had some bizarre configuration, but it
-  // was difficult to diagnose because the symptom is "no messages of any
-  // kind").
-  while (ob_get_level() > 0) {
-    ob_end_clean();
-  }
+  // Now, load libphutil.
+
+  $root = dirname(dirname(__FILE__));
+  require_once $root.'/src/__phutil_library_init__.php';
 }
 
 __phutil_init_script__();
