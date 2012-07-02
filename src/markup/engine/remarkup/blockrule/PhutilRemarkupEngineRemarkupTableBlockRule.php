ce0c1f2b632687e45ecc83ccf43cd0dba1cdeec6
epriestley <git@epriestley.com>
2012-07-02 14:42:51 -0700

    Add <table> syntax to Remarkup
    Summary:
    Adds basic table syntax. I went with just HTML table syntax since it's straightforward and familiar and all the other syntaxes (in mediawiki, trac, etc) seem awful.
    
    It's safe to do this since we parse the talbe markup itself very strictly, and don't actually use the tags the user types.
    
    Test Plan: {F13683}
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D2902

---
+++ src/markup/engine/remarkup/blockrule/PhutilRemarkupEngineRemarkupTableBlockRule.php
@@ -0,0 +1,131 @@
+<?php
+
+/*
+ * Copyright 2012 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @group markup
+ */
+final class PhutilRemarkupEngineRemarkupTableBlockRule
+  extends PhutilRemarkupEngineBlockRule {
+
+  public function getBlockPattern() {
+    return '/^<table>/i';
+  }
+
+  public function shouldContinueWithBlock($block, $last_block) {
+    // Until we consume a '</table>', keep merging blocks.
+    if (preg_match('@</table>$@i', $last_block)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public function shouldMergeBlocks() {
+    return true;
+  }
+
+  public function markupText($text) {
+    $matches = array();
+
+    if (!preg_match('@^<table>(.*)</table>$@si', $text, $matches)) {
+      return $this->fail(
+        $text,
+        'Bad table (expected <table>...</table>)');
+    }
+
+    $body = $matches[1];
+
+    $row_fragment = '(?:\s*<tr>(.*)</tr>\s*)';
+    $cell_fragment = '(?:\s*<(td|th)>(.*)</(?:td|th)>\s*)';
+
+    // Test that the body contains only valid rows.
+    if (!preg_match('@^'.$row_fragment.'+$@Usi', $body)) {
+      return $this->fail(
+        $body,
+        'Bad table syntax (expected rows <tr>...</tr>)');
+    }
+
+    // Capture the rows.
+    $row_regex = '@'.$row_fragment.'@Usi';
+    if (!preg_match_all($row_regex, $body, $matches, PREG_SET_ORDER)) {
+      throw new Exception(
+        "Bug in Remarkup tables, parsing fails for input: ".$text);
+    }
+
+    $out_rows = array();
+
+    $rows = $matches;
+    foreach ($rows as $row) {
+      $content = $row[1];
+
+      // Test that the row contains only valid cells.
+      if (!preg_match('@^'.$cell_fragment.'+$@Usi', $content)) {
+        return $this->fail(
+          $content,
+          'Bad table syntax (expected cells <td>...</td>)');
+      }
+
+      // Capture the cells.
+      $cell_regex = '@'.$cell_fragment.'@Usi';
+      if (!preg_match_all($cell_regex, $content, $matches, PREG_SET_ORDER)) {
+        throw new Exception(
+          "Bug in Remarkup tables, parsing fails for input: ".$text);
+      }
+
+      $out_cells = array();
+      foreach ($matches as $cell) {
+        $cell_type = $cell[1];
+        $cell_content = $cell[2];
+
+        $out_cells[] = array(
+          'type'      => $cell_type,
+          'content'   => $cell_content,
+        );
+      }
+
+      $out_rows[] = array(
+        'type'    => 'tr',
+        'content' => $out_cells,
+      );
+    }
+
+    $out = array();
+    $out[] = '<table class="remarkup-table">';
+    foreach ($out_rows as $row) {
+      $out[] = '<'.$row['type'].'>';
+      foreach ($row['content'] as $cell) {
+        $out[] = '<'.$cell['type'].'>';
+        $out[] = $this->applyRules($cell['content']);
+        $out[] = '</'.$cell['type'].'>';
+      }
+      $out[] = '</'.$row['type'].'>';
+    }
+    $out[] = '</table>';
+
+    return implode("\n", $out);
+  }
+
+  private function fail($near, $message) {
+    return
+      '<div style="color: red;">'.
+        phutil_escape_html($message).' near: '.
+        phutil_escape_html(phutil_utf8_shorten($near, 32000)).
+      '</div>';
+  }
+
+}
