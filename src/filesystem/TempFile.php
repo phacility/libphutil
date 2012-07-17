5363f990f83464fce64de3dae46f7645853012f0
epriestley <git@epriestley.com>
2012-07-17 16:15:25 -0700

    Fix various TempFile issues
    Summary:
    This class is a mess, I think it hasn't been touched since it came out of FB.
    
      - $dir parameter to constructor does nothing.
      - preserve() does nothing.
      - Fix documentation.
      - Use more Filesystem.
    
    Test Plan: Used setPreserveFile() in a future diff.
    
    Reviewers: btrahan, vrana
    
    Reviewed By: btrahan
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D2996

--- src/filesystem/TempFile.php
+++ src/filesystem/TempFile.php
@@ -17,34 +17,40 @@
  */
 
 /**
- * Simple wrapper to guarantee temp file will be deleted on
+ * Simple wrapper to create a temporary file and guarantee it will be deleted on
  * object destruction. Used like a string to path:
  *
  *    $temp = new TempFile();
  *    Filesystem::writeFile($temp, 'Hello World');
- *    echo "wrote data to path: {$temp}";
- *
- * The extra parameter $dir can be used to specify which temporary diretcory
- * you want to create your file inside.  If not specified, it will create a
- * temp directory for your temp file.
- *
- * The class tries to be smart about what you mean to do.  For example, if you
- * don't specify a $dir to write to but give a filename, then it thinks you need
- * a temporary file of that exact filename.  If however you do specify a
- * temporary directory to write to, then it assumes you can't guarantee that
- * $filename is unique and that you want this class to handle the automatic
- * destruction of $filename when done.
+ *    echo "Wrote data to path: ".$temp;
  *
  * Throws Filesystem exceptions for errors.
+ *
+ * @task  create    Creating a Temporary File
+ * @task  config    Configuration
+ * @task  internal  Internals
  *
  * @group filesystem
  */
 final class TempFile {
-  protected $dir;
-  protected $file;
 
-  public function __construct($filename = null, $dir = null) {
-    $this->dir  = Filesystem::createTemporaryDirectory();
+  private $dir;
+  private $file;
+  private $preserve;
+
+/* -(  Creating a Temporary File  )------------------------------------------ */
+
+
+  /**
+   * Create a new temporary file.
+   *
+   * @param string? Filename hint. This is useful if you intend to edit the
+   *                file with an interactive editor, so the user's editor shows
+   *                "commit-message" instead of "p3810hf-1z9b89bas".
+   * @task create
+   */
+  public function __construct($filename = null) {
+    $this->dir = Filesystem::createTemporaryDirectory();
     if ($filename === null) {
       $this->file = tempnam($this->dir, getmypid().'-');
     } else {
@@ -54,20 +60,56 @@ final class TempFile {
     Filesystem::writeFile($this, '');
   }
 
+
+/* -(  Configuration  )------------------------------------------------------ */
+
+
+  /**
+   * Normally, the file is deleted when this object passes out of scope. You
+   * can set it to be preserved instead.
+   *
+   * @param bool True to preserve the file after object destruction.
+   * @return this
+   * @task config
+   */
+  public function setPreserveFile($preserve) {
+    $this->preserve = $preserve;
+    return $this;
+  }
+
+
+/* -(  Internals  )---------------------------------------------------------- */
+
+
+  /**
+   * Get the path to the temporary file. Normally you can just use the object
+   * in a string context.
+   *
+   * @return string Absolute path to the temporary file.
+   * @task internal
+   */
   public function __toString() {
     return $this->file;
   }
 
+
+  /**
+   * When the object is destroyed, it destroys the temporary file. You can
+   * change this behavior with @{method:setPreserveFile}.
+   *
+   * @task internal
+   */
   public function __destruct() {
-    Filesystem::remove($this->dir);
-    // Note that the function tempnam() doesn't guarantee it will return a
-    // file inside the dir you passed to the function.
-    if (file_exists($this->file)) {
-      unlink($this->file);
+    if ($this->preserve) {
+      return;
     }
+    Filesystem::remove($this->dir);
+
+    // NOTE: tempnam() doesn't guarantee it will return a file inside the
+    // directory you passed to the function, so we make sure to nuke the file
+    // explicitly.
+
+    Filesystem::remove($this->file);
   }
 
-  public function preserve() {
-    $this->destroy = false;
-  }
 }
