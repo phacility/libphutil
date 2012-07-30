915f92d2a964f258892017d7d40d94ee839599cd
epriestley <git@epriestley.com>
2012-07-30 05:43:28 -0700

    Fix warning when selecting descendants of a class with no descendants
    Summary: If you try to select the descendants of a class that has no children, we currently raise a warning. Handle this condition properly.
    
    Test Plan: Selected descendants of a childless class.
    
    Reviewers: vrana, btrahan
    
    Reviewed By: btrahan
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D3097

--- src/symbols/PhutilSymbolLoader.php
+++ src/symbols/PhutilSymbolLoader.php
@@ -339,6 +339,12 @@ final class PhutilSymbolLoader {
    */
   private function selectDescendantsOf(array $tree, $root) {
     $result = array();
+
+    if (empty($tree[$root])) {
+      // No known descendants.
+      return array();
+    }
+
     foreach ($tree[$root] as $child) {
       $result[$child] = true;
       if (!empty($tree[$child])) {
