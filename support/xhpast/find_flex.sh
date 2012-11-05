c6d16500303f69854b2f7653bc32236e00981704
vrana <jakubv@fb.com>
2012-11-04 20:45:56 -0800

    Recognize flex35 in xhpast
    Test Plan:
      support/xhpast/ $ make scanner
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, Korvin
    
    Differential Revision: https://secure.phabricator.com/D3888

--- support/xhpast/find_flex.sh
+++ support/xhpast/find_flex.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 FLEX=`which flex35 2>/dev/null || which flex 2>/dev/null`
 if [ $? -ne 0 ]
 then
@@ -15,9 +14,9 @@ then
   echo "ERROR: the version of flex on this system is too old." 1>&2
   echo "Upgrade to at least 2.5.35 (which is NEWER than 2.5.4)." 1>&2
   exit 1;
-fi 
+fi
 
-if [[ $FLEXVERSION != "flex 2.5.35" ]]
+if [ "$FLEXVERSION" != "flex 2.5.35" -a "$FLEXVERSION" != "flex35 2.5.35" ]
 then
   echo "WARNING: the version of flex on this system may be incorrect." 1>&2
   echo "If you receive errors, try upgrading to 2.5.35 or newer." 1>&2
