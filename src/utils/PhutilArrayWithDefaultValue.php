3c969f73ebbccde6df79d5271265ba751f2e7f26
epriestley <git@epriestley.com>
2012-12-07 07:12:58 -0800

    Implement PhutilArrayWithDefaultValue to simplify building a map with a count or mask
    Summary: This can greatly simplify some of the code in my `getWorkingCopyStatus()` changes for D4049. See comments for details.
    
    Test Plan: Ran unit tests.
    
    Reviewers: vrana
    
    Reviewed By: vrana
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D4092

---
+++ src/utils/PhutilArrayWithDefaultValue.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Array-like object with a default value that is populated when nonexistent
+ * keys are accessed. This is particularly useful to avoid constructs like this:
+ *
+ *   $counts = array();
+ *   foreach ($fruit as $fruit) {
+ *     if (!isset($counts[$fruit])) {
+ *       $counts[$fruit] = 0;
+ *     }
+ *     $counts[$fruit] += 1;
+ *   }
+ *
+ * Instead, you can use PhutilArrayWithDefaultValue:
+ *
+ *   $counts = new PhutilArrayWithDefaultValue();
+ *   foreach ($fruits as $fruit) {
+ *     $counts[$fruit] += 1;
+ *   }
+ *
+ * NOTE: You must use the `+= 1` operator, not `++`, because PHP is the crown
+ * jewel of programming languages.
+ *
+ * Normally, the default value is `0` to allow the object to be used to count
+ * things or build bitmasks. You can change the default with
+ * @{method:setDefaultValue}.
+ *
+ * @group util
+ */
+final class PhutilArrayWithDefaultValue extends PhutilArray {
+
+  private $defaultValue = 0;
+
+  public function __construct(array $initial_value = array()) {
+    $this->data = $initial_value;
+  }
+
+  public function setDefaultValue($default_value) {
+    $this->defaultValue = $default_value;
+    return $this;
+  }
+
+  public function offsetExists($key) {
+    return true;
+  }
+
+  public function offsetGet($key) {
+    if (!parent::offsetExists($key)) {
+      $this->offsetSet($key, $this->defaultValue);
+    }
+    return parent::offsetGet($key);
+  }
+
+}
