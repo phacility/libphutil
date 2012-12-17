5504fa53f62c7b00df009d288c66e9acefa46c01
epriestley <git@epriestley.com>
2012-12-17 13:53:42 -0800

    Don't require non-boundary on trailing backtick when matching monospaced remarkup rule
    Summary: Fixes T2200; see that task for discussion.
    
    Test Plan: See unit tests.
    
    Reviewers: btrahan, vrana
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T2200
    
    Differential Revision: https://secure.phabricator.com/D4218

--- src/markup/engine/remarkup/markuprule/PhutilRemarkupRuleMonospace.php
+++ src/markup/engine/remarkup/markuprule/PhutilRemarkupRuleMonospace.php
@@ -7,8 +7,14 @@ final class PhutilRemarkupRuleMonospace
   extends PhutilRemarkupRule {
 
   public function apply($text) {
+    // NOTE: We don't require a trailing non-boundary on the backtick syntax,
+    // to permit the use case of naming and pluralizing a class, like
+    // "Load all the `PhutilArray`s and then iterate over them." In theory, the
+    // required \B on the leading backtick should protect us from most
+    // collateral damage.
+
     return preg_replace_callback(
-      '@##([\s\S]+?)##|\B`(.+?)`\B@',
+      '@##([\s\S]+?)##|\B`(.+?)`@',
       array($this, 'markupMonospacedText'),
       $text);
   }
