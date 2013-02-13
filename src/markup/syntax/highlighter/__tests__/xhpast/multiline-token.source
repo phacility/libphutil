phacility/libphutil

commit 1c90c18fa590376f0dc645b9bd39e2a7208c509b

Return safe HTML from syntax highlighters
Summary: I wonder what the perf impact of this is.

Test Plan: None yet.

Reviewers: epriestley

Reviewed By: epriestley

CC: aran, Korvin

Differential Revision: https://secure.phabricator.com/D4873

src/markup/syntax/highlighter/xhpast/PhutilXHPASTSyntaxHighlighterFuture.php
@@ -50,19 +50,15 @@ final class PhutilXHPASTSyntaxHighlighterFuture extends FutureProxy {
 
     $out = array();
     foreach ($tokens as $key => $token) {
-      $value = phutil_escape_html($token->getValue());
+      $value = $token->getValue();
       $class = null;
       $multi = false;
-      $attrs = '';
+      $attrs = array();
       if (isset($interesting_symbols[$key])) {
         $sym = $interesting_symbols[$key];
         $class = $sym[0];
-        if (isset($sym['context'])) {
-          $attrs = $attrs.' data-symbol-context="'.$sym['context'].'"';
-        }
-        if (isset($sym['symbol'])) {
-          $attrs = $attrs.' data-symbol-name="'.$sym['symbol'].'"';
-        }
+        $attrs['data-symbol-context'] = idx($sym, 'context');
+        $attrs['data-symbol-name'] = idx($sym, 'symbol');
       } else {
         switch ($token->getTypeName()) {
           case 'T_WHITESPACE':
@@ -99,7 +95,7 @@ final class PhutilXHPASTSyntaxHighlighterFuture extends FutureProxy {
               'false' => true,
               'null' => true,
             );
-            if (isset($magic[$value])) {
+            if (isset($magic[strtolower($value)])) {
               $class = 'k';
               break;
             }
@@ -110,21 +106,20 @@ final class PhutilXHPASTSyntaxHighlighterFuture extends FutureProxy {
             break;
         }
       }
-      if ($class) {
-        $l = '<span class="'.$class.'"'.$attrs.'>';
-        $r = '</span>';
 
-        $value = $l.$value.$r;
+      if ($class) {
+        $attrs['class'] = $class;
         if ($multi) {
           // If the token may have multiple lines in it, make sure each
           // <span> crosses no more than one line so the lines can be put
           // in a table, etc., later.
-          $value = str_replace(
-            "\n",
-            $r."\n".$l,
-            $value);
+          $value = explode("\n", $value);
+        } else {
+          $value = array($value);
         }
-        $out[] = $value;
+        foreach ($value as $val) {
+          $out[] = phutil_tag('span', $attrs, $val);
+        }
       } else {
         $out[] = $value;
       }
@@ -134,7 +129,7 @@ final class PhutilXHPASTSyntaxHighlighterFuture extends FutureProxy {
       array_shift($out);
     }
 
-    return rtrim(implode('', $out));
+    return phutil_implode_html('', $out);
   }
 
   private function findInterestingSymbols(XHPASTNode $root) {

src/markup/syntax/highlighter/__tests__/PhutilXHPASTSyntaxHighlighterTestCase.php
@@ -19,11 +19,11 @@ final class PhutilXHPASTSyntaxHighlighterTestCase extends PhutilTestCase {
   public function testBuiltinClassnames() {
     $this->assertEqual(
       $this->read('builtin-classname.expect'),
-      $this->highlight($this->read('builtin-classname.source')),
+      (string)$this->highlight($this->read('builtin-classname.source')),
       'Builtin classnames should not be marked as linkable symbols.');
     $this->assertEqual(
       $this->read('trailing-comment.expect'),
-      $this->highlight($this->read('trailing-comment.source')),
+      (string)$this->highlight($this->read('trailing-comment.source')),
       'Trailing comments should not be dropped.');
   }
 

src/__phutil_library_map__.php
@@ -280,6 +280,7 @@ phutil_register_library_map(array(
     'phutil_get_library_name_for_root' => 'moduleutils/moduleutils.php',
     'phutil_get_library_root' => 'moduleutils/moduleutils.php',
     'phutil_get_library_root_for_path' => 'moduleutils/moduleutils.php',
+    'phutil_implode_html' => 'markup/render.php',
     'phutil_is_utf8' => 'utils/utf8.php',
     'phutil_passthru' => 'future/exec/execx.php',
     'phutil_render_tag' => 'markup/render.php',

src/markup/render.php
@@ -91,6 +91,17 @@ function phutil_safe_html($string) {
 }
 
 /**
+ * HTML safe version of implode().
+ *
+ * @group markup
+ */
+function phutil_implode_html($glue, array $pieces) {
+  $glue = phutil_escape_html($glue);
+  $pieces = array_map('phutil_escape_html', $pieces);
+  return phutil_safe_html(implode($glue, $pieces));
+}
+
+/**
  * Format a HTML code. This function behaves like sprintf(), except that all
  * the normal conversions (like %s) will be properly escaped.
  *

src/markup/syntax/highlighter/PhutilConsoleSyntaxHighlighter.php
@@ -35,21 +35,19 @@ final class PhutilConsoleSyntaxHighlighter {
         '(#.*|\\\\)?$@';
 
       if (preg_match($pattern, $line, $matches)) {
-        $line = '';
-        if ($matches[1]) {
-          $line .= phutil_escape_html($matches[1]);
-        }
-        $line .= '<span class="gp">'.phutil_escape_html($matches[2]).'</span>';
-        if (!empty($matches[3])) {
-          $line .= '<span class="k">'.phutil_escape_html($matches[3]).'</span>';
-        }
-        $lines[$key] = $line;
+        $lines[$key] = hsprintf(
+          '%s<span class="gp">%s</span>%s',
+          $matches[1],
+          $matches[2],
+          (!empty($matches[3])
+            ? hsprintf('<span class="k">%s</span>', $matches[3])
+            : ''));
         $in_command = (idx($matches, 3) == '\\');
       } else {
-        $lines[$key] = '<span class="go">'.phutil_escape_html($line).'</span>';
+        $lines[$key] = hsprintf('<span class="go">%s</span>', $line);
       }
     }
-    $lines = implode("\n", $lines);
+    $lines = phutil_implode_html("\n", $lines);
 
     return new ImmediateFuture($lines);
   }

src/markup/syntax/highlighter/PhutilDefaultSyntaxHighlighter.php
@@ -10,7 +10,7 @@ final class PhutilDefaultSyntaxHighlighter {
   }
 
   public function getHighlightFuture($source) {
-    $result = phutil_escape_html($source);
+    $result = hsprintf('%s', $source);
     return new ImmediateFuture($result);
   }
 }

src/markup/syntax/highlighter/PhutilDivinerSyntaxHighlighter.php
@@ -40,6 +40,7 @@ final class PhutilDivinerSyntaxHighlighter {
     // Highlight stuff that looks like headers.
     $source = $this->highlightPattern('/^=(.*)$/m', $source, 'nv');
 
+    $source = phutil_safe_html($source);
     return new ImmediateFuture($source);
   }
 

src/markup/syntax/highlighter/PhutilLexerSyntaxHighlighter.php
@@ -30,26 +30,14 @@ final class PhutilLexerSyntaxHighlighter extends PhutilSyntaxHighlighter {
     $result = array();
     foreach ($tokens as $token) {
       list($type, $value, $context) = $token;
-      $value = phutil_escape_html($value);
 
       $data_name = null;
       switch ($type) {
         case 'nc':
         case 'nf':
         case 'na':
-          $data_name = ' data-symbol-name="'.$value.'"';
+          $data_name = $value;
           break;
-      }
-
-      $data_context = null;
-      if ($context) {
-        $context = phutil_escape_html($context);
-        $data_context = ' data-symbol-context="'.$context.'"';
-      }
-
-      $class_name = null;
-      if ($type) {
-        $class_name = ' class="'.$type.'"';
       }
 
       if (strpos($value, "\n") !== false) {
@@ -59,11 +47,15 @@ final class PhutilLexerSyntaxHighlighter extends PhutilSyntaxHighlighter {
       }
       foreach ($value as $part) {
         if (strlen($part)) {
-          if ($class_name) {
-            $result[] =
-              '<span'.$class_name.$data_context.$data_name.'>'.
-                $part.
-              '</span>';
+          if ($type) {
+            $result[] = phutil_tag(
+              'span',
+              array(
+                'class' => $type,
+                'data-symbol-context' => $context,
+                'data-symbol-name' => $data_name,
+              ),
+              $part);
           } else {
             $result[] = $part;
           }
@@ -75,7 +67,7 @@ final class PhutilLexerSyntaxHighlighter extends PhutilSyntaxHighlighter {
       array_pop($result);
     }
 
-    $result = implode('', $result);
+    $result = phutil_implode_html('', $result);
 
     return new ImmediateFuture($result);
   }

commit 9041dd194e654a1102aa5d1bd5ee9d750e65f91c

Fix issue with highlighting multi-line tokens in libphutil
Summary: Broken by D4873, which accidentally discarded newlines.

Test Plan: Added unit test; made it pass.

Reviewers: vrana

Reviewed By: vrana

CC: aran

Differential Revision: https://secure.phabricator.com/D4941

src/markup/syntax/highlighter/xhpast/PhutilXHPASTSyntaxHighlighterFuture.php
@@ -113,7 +113,7 @@ final class PhutilXHPASTSyntaxHighlighterFuture extends FutureProxy {
           // If the token may have multiple lines in it, make sure each
           // <span> crosses no more than one line so the lines can be put
           // in a table, etc., later.
-          $value = explode("\n", $value);
+          $value = phutil_split_lines($value, $retain_endings = true);
         } else {
           $value = array($value);
         }

src/markup/syntax/highlighter/__tests__/PhutilXHPASTSyntaxHighlighterTestCase.php
@@ -25,6 +25,10 @@ final class PhutilXHPASTSyntaxHighlighterTestCase extends PhutilTestCase {
       $this->read('trailing-comment.expect'),
       (string)$this->highlight($this->read('trailing-comment.source')),
       'Trailing comments should not be dropped.');
+    $this->assertEqual(
+      $this->read('multiline-token.expect'),
+      (string)$this->highlight($this->read('multiline-token.source')),
+      'Multi-line tokens should be split across lines.');
   }
 
 }

src/markup/syntax/highlighter/__tests__/xhpast/multiline-token.expect
@@ -1,0 +1,5 @@
+<span class="o">&lt;?php
+</span>
+<span class="c">/* this comment
+</span><span class="c">extends across
+</span><span class="c">multiple lines */</span>

src/markup/syntax/highlighter/__tests__/xhpast/multiline-token.source
@@ -1,0 +1,5 @@
+<?php
+
+/* this comment
+extends across
+multiple lines */