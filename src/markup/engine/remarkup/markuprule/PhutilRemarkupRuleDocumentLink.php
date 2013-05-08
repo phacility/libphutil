b03bdc33a4a1126737cd1b664fc7014bca4ea0ca
epriestley <git@epriestley.com>
2013-05-07 18:48:06 -0700

    Parse most `[name](href)` Markdown-style links in Remarkup
    Summary: Markdown has `[name](href)`-style links which we currently don't parse because they can occur naturally in discussions of source code (instead, we support Mediawiki-style links). Apply a few heuristics to reject likely accidental matches and support reasonable forms of this style. Ref T2849.
    
    Test Plan: Added unit tests.
    
    Reviewers: btrahan, jevripio
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T2849
    
    Differential Revision: https://secure.phabricator.com/D5857

--- src/markup/engine/remarkup/markuprule/PhutilRemarkupRuleDocumentLink.php
+++ src/markup/engine/remarkup/markuprule/PhutilRemarkupRuleDocumentLink.php
@@ -8,9 +8,16 @@ final class PhutilRemarkupRuleDocumentLink
 
   public function apply($text) {
 
+    // Handle mediawiki-style links: [[ href | name ]]
     $text = preg_replace_callback(
       '@\B\\[\\[([^|\\]]+)(?:\\|([^\\]]+))?\\]\\]\B@U',
       array($this, 'markupDocumentLink'),
+      $text);
+
+    // Handle markdown-style links: [name](href)
+    $text = preg_replace_callback(
+      '@\B\\[([^\\]]+)\\]\\(([^\\)]+)\\)\B@U',
+      array($this, 'markupAlternateLink'),
       $text);
 
     return $text;
@@ -22,10 +29,10 @@ final class PhutilRemarkupRuleDocumentLink
       if (strncmp($link, '/', 1) == 0) {
         $text = rtrim($this->getEngine()->getConfig('uri.prefix'), '/').$text;
       }
-      if ($link != $name) {
-        $text .= ' | '.$name;
+      if ($link == $name) {
+        return $text;
       }
-      return '[[ '.$text.' ]]';
+      return $name.' <'.$text.'>';
     }
 
     if ($this->getEngine()->getState('toc')) {
@@ -39,6 +46,33 @@ final class PhutilRemarkupRuleDocumentLink
         ),
         $name);
     }
+  }
+
+  public function markupAlternateLink($matches) {
+    $uri = trim($matches[2]);
+
+    // NOTE: We apply some special rules to avoid false positives here. The
+    // major concern is that we do not want to convert `x[0][1](y)` in a
+    // discussion about C source code into a link. To this end, we:
+    //
+    //   - Don't match at word boundaries;
+    //   - require the URI to contain a "/" character; and
+    //   - reject URIs which being with a quote character.
+
+    if ($uri[0] == '"' || $uri[0] == "'" || $uri[0] == '`') {
+      return $matches[0];
+    }
+
+    if (strpos($uri, '/') === false) {
+      return $matches[0];
+    }
+
+    return $this->markupDocumentLink(
+      array(
+        $matches[0],
+        $matches[2],
+        $matches[1],
+      ));
   }
 
   public function markupDocumentLink($matches) {
