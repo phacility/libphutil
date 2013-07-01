70dad82483bf4f03fe0108a7623752756d78232d
epriestley <git@epriestley.com>
2013-07-01 12:30:40 -0700

    Minor, give the monospace rule an earlier priority.

--- src/markup/engine/remarkup/markuprule/PhutilRemarkupRuleMonospace.php
+++ src/markup/engine/remarkup/markuprule/PhutilRemarkupRuleMonospace.php
@@ -6,6 +6,10 @@
 final class PhutilRemarkupRuleMonospace
   extends PhutilRemarkupRule {
 
+  public function getPriority() {
+    return 100.0;
+  }
+
   public function apply($text) {
     // NOTE: We don't require a trailing non-boundary on the backtick syntax,
     // to permit the use case of naming and pluralizing a class, like
