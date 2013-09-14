c0d2fd4784b34607f07520cce1220344c603b9fc
Gareth Evans <garoevans@gmail.com>
2013-09-14 15:15:44 -0700

    Allow int to trigger ordered list remarkup
    Summary:
    A line starting with `1.`, `1/` or `1)` can trigger the start of an ordered list.
    
    Once in the list `[0-9]+[./)]` will continue matching the new syntax.
    
    Test Plan: Unit test
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: Korvin, aran
    
    Maniphest Tasks: T3797
    
    Differential Revision: https://secure.phabricator.com/D6994

--- src/markup/engine/remarkup/blockrule/PhutilRemarkupEngineRemarkupListBlockRule.php
+++ src/markup/engine/remarkup/blockrule/PhutilRemarkupEngineRemarkupListBlockRule.php
@@ -18,21 +18,29 @@ final class PhutilRemarkupEngineRemarkupListBlockRule
     $num_lines = 0;
 
     while (isset($lines[$cursor])) {
-      if (preg_match(self::BLOCK_PATTERN, $lines[$cursor])) {
-        $num_lines++;
-        $cursor++;
-        continue;
-      }
+      if (!$num_lines) {
+        if (preg_match(self::START_BLOCK_PATTERN, $lines[$cursor])) {
+          $num_lines++;
+          $cursor++;
+          continue;
+        }
+      } else {
+        if (preg_match(self::CONT_BLOCK_PATTERN, $lines[$cursor])) {
+          $num_lines++;
+          $cursor++;
+          continue;
+        }
 
-      if ($num_lines && strlen(trim($lines[$cursor]))
-        && strlen($lines[$cursor]) !== strlen(ltrim($lines[$cursor]))) {
-        $num_lines++;
-        $cursor++;
-        continue;
-      }
+        if (strlen(trim($lines[$cursor]))
+          && strlen($lines[$cursor]) !== strlen(ltrim($lines[$cursor]))) {
+          $num_lines++;
+          $cursor++;
+          continue;
+        }
 
-      if ($num_lines && !strlen(trim($lines[$cursor]))) {
-        $num_lines++;
+        if (!strlen(trim($lines[$cursor]))) {
+          $num_lines++;
+        }
       }
 
       break;
@@ -46,7 +54,8 @@ final class PhutilRemarkupEngineRemarkupListBlockRule
    * the stack.
    */
   const MAXIMUM_LIST_NESTING_DEPTH = 12;
-  const BLOCK_PATTERN = '/^\s*[-*#]+\s+/';
+  const START_BLOCK_PATTERN = '@^\s*(?:[-*#]+|1[./)])\s+@';
+  const CONT_BLOCK_PATTERN = '@^\s*(?:[-*#]+|[0-9]+[./)])\s+@';
 
   public function markupText($text) {
 
@@ -69,10 +78,12 @@ final class PhutilRemarkupEngineRemarkupListBlockRule
     // un-indent every item by the minimum indentation level for the whole
     // block before we begin parsing.
 
+    $regex = self::START_BLOCK_PATTERN;
     $min_space = PHP_INT_MAX;
-    foreach ($lines as $line) {
+    foreach ($lines as $ii => $line) {
       $matches = null;
-      if (preg_match(self::BLOCK_PATTERN, $line)) {
+      if (preg_match($regex, $line)) {
+        $regex = self::CONT_BLOCK_PATTERN;
         if (preg_match('/^(\s+)/', $line, $matches)) {
           $space = strlen($matches[1]);
         } else {
@@ -81,9 +92,12 @@ final class PhutilRemarkupEngineRemarkupListBlockRule
         $min_space = min($min_space, $space);
       }
     }
+
+    $regex = self::START_BLOCK_PATTERN;
     if ($min_space) {
       foreach ($lines as $key => $line) {
-        if (preg_match(self::BLOCK_PATTERN, $line)) {
+        if (preg_match($regex, $line)) {
+          $regex = self::CONT_BLOCK_PATTERN;
           $lines[$key] = substr($line, $min_space);
         }
       }
@@ -110,8 +124,10 @@ final class PhutilRemarkupEngineRemarkupListBlockRule
     //   );
 
     $item = array();
+    $regex = self::START_BLOCK_PATTERN;
     foreach ($lines as $line) {
-      if (preg_match(self::BLOCK_PATTERN, $line)) {
+      if (preg_match($regex, $line)) {
+        $regex = self::CONT_BLOCK_PATTERN;
         if ($item) {
           $items[] = $item;
           $item = array();
@@ -162,7 +178,7 @@ final class PhutilRemarkupEngineRemarkupListBlockRule
         $depth = 0;
       }
 
-      if (preg_match('/^\s*#/', $item)) {
+      if (preg_match('/^\s*(?:#|[0-9])/', $item)) {
         $style = '#';
       } else {
         $style = '-';
@@ -172,7 +188,7 @@ final class PhutilRemarkupEngineRemarkupListBlockRule
       // has typed only " " or " -"), treat the line as containing no text.
       // This prevents newly added items from rendering with a bullet and
       // the text "-", e.g.
-      $text = preg_replace(self::BLOCK_PATTERN, '', $item);
+      $text = preg_replace(self::CONT_BLOCK_PATTERN, '', $item);
       if ($text == $item) {
         $text = '';
       }
