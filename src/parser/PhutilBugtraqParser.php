7eccbcfe5f53dc5866812b686998d966453f6133
epriestley <git@epriestley.com>
2013-09-18 10:12:45 -0700

    Implement Bugtraq as a standalone parse step
    Summary:
    Ref T3840. I wasn't able to figure the bug in this logic out initially and it doesn't //really// belong on PhabricatorRepository. Separate it out and write tests for it.
    
    The current logic lives here:
    
    https://secure.phabricator.com/diffusion/P/browse/master/src/applications/repository/storage/PhabricatorRepository.php;d3de57716a4fc528$695-744
    
    The bug was that the logic basically does this:
    
      foreach (capturing match) {
        foreach (array_reverse(selection match)) {
          substr_replace(...);
        }
      }
    
    So we do the selection matches in reverse order (which avoids disrupting the offset), but the capturing matches in forward order. So if you have text like this:
    
      Issues: 123
      Issues: 345
    
    ...everything will end up garbled. Instead, collect all replacements and then do them all in reverse order.
    
    The next diff will replace the logic in Phabricator with a call to this logic.
    
    Test Plan: Unit tests.
    
    Reviewers: btrahan, vrana
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T3840
    
    Differential Revision: https://secure.phabricator.com/D7021

---
+++ src/parser/PhutilBugtraqParser.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * Replaces ticket or issue IDs in a block of text with links. This mechanism
+ * is based on (and compatible with) the Bugtraq system in TortiseSVN. See
+ *
+ *   http://tortoisesvn.net/docs/release/TortoiseSVN_en/tsvn-dug-bugtracker.html
+ *
+ * For example, to link ticket IDs formatted like this:
+ *
+ *   Issues: 123, 345
+ *
+ * ...you can use:
+ *
+ *   $message = id(new PhutilBugtraqParser())
+ *     ->setBugtraqPattern('http://bugs.com/%BUGID%')
+ *     ->setBugtraqCaptureExpression('/[Ii]ssues?:?(\s*,?\s*#\d+)+/')
+ *     ->setBugtraqSelectExpression('/(\d+)/')
+ *     ->processCorpus($message);
+ *
+ * This will produce:
+ *
+ *   Issues: http://bugs.com/123, http://bugs.com/345
+ *
+ */
+final class PhutilBugtraqParser {
+
+  private $bugtraqPattern;
+  private $bugtraqCaptureExpression;
+  private $bugtraqSelectExpression;
+
+  public function setBugtraqPattern($pattern) {
+    $this->bugtraqPattern = $pattern;
+    return $this;
+  }
+
+  public function setBugtraqCaptureExpression($regex) {
+    PhutilTypeSpec::newFromString('regex')->check($regex);
+
+    $this->bugtraqCaptureExpression = $regex;
+    return $this;
+  }
+
+  public function setBugtraqSelectExpression($regex) {
+    PhutilTypeSpec::newFromString('regex')->check($regex);
+
+    $this->bugtraqSelectExpression = $regex;
+    return $this;
+  }
+
+  public function processCorpus($corpus) {
+    $regexp = $this->bugtraqCaptureExpression;
+    $matches = null;
+    $flags = PREG_SET_ORDER | PREG_OFFSET_CAPTURE;
+
+    // First, find all the matching text ranges. We do this up front because
+    // when we do the replacement pass at the end, the whole thing needs to go
+    // in reverse order.
+
+    preg_match_all($regexp, $corpus, $matches, $flags);
+    $captures = array();
+    foreach ($matches as $match) {
+      list($captured_text, $captured_offset) = $match[0];
+      $captures[] = array(
+        'text' => $captured_text,
+        'at' => $captured_offset,
+      );
+    }
+
+    // Find the actual bug IDs. If there's a selection expression, we use that
+    // to pick bug IDs out of a larger context. For example, the syntax may be:
+    //
+    //   Issues: 123, 124
+    //
+    // In this case, "123" and "124" are the issue IDs, and could be selected
+    // with an expression like:
+    //
+    //   /(\d+)/
+    //
+    // If there's no selection expression, we use the entire match.
+
+    $bug_ids = array();
+    $select_regexp = $this->bugtraqSelectExpression;
+    foreach ($captures as $capture) {
+      $captured_text = $capture['text'];
+      $captured_offset = $capture['at'];
+
+      if (strlen($select_regexp)) {
+        $selections = null;
+        preg_match_all(
+          $select_regexp,
+          $captured_text,
+          $selections,
+          PREG_OFFSET_CAPTURE);
+
+        foreach ($selections[1] as $selection) {
+          $bug_ids[] = array(
+            'text' => $selection[0],
+            'at' => $captured_offset + $selection[1],
+          );
+        }
+      } else {
+        $bug_ids[] = array(
+          'text' => $captured_text,
+          'at' => $captured_offset,
+        );
+      }
+    }
+
+    // Now that we have all the bug IDs to replace, replace them (in reverse
+    // order, so the offsets don't get messed up).
+    $bug_ids = array_reverse($bug_ids);
+    foreach ($bug_ids as $bug) {
+      $new_text = str_replace(
+        '%BUGID%',
+        $bug['text'],
+        $this->bugtraqPattern);
+
+      $corpus = substr_replace(
+        $corpus,
+        $new_text,
+        $bug['at'],
+        strlen($bug['text']));
+    }
+
+    return $corpus;
+  }
+
+}
