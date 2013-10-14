247cc189f6c8f023fb4662821d8de3da1239fa97
epriestley <git@epriestley.com>
2013-10-14 14:34:37 -0700

    Add an auth adapter for Mozilla Persona
    Summary: Ref T3958. Adds the adapter for Persona.
    
    Test Plan: See next revision.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T3958
    
    Differential Revision: https://secure.phabricator.com/D7312

---
+++ src/auth/PhutilAuthAdapterPersona.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Adapter for Mozilla's Persona.
+ */
+final class PhutilAuthAdapterPersona extends PhutilAuthAdapter {
+
+  private $audience;
+  private $assertion;
+  private $accountData;
+
+  public function setAssertion($assertion) {
+    $this->assertion = $assertion;
+    return $this;
+  }
+
+  public function setAudience($audience) {
+    $this->audience = $audience;
+    return $this;
+  }
+
+  public function getAdapterDomain() {
+    return 'verifier.login.persona.org';
+  }
+
+  public function getAdapterType() {
+    return 'persona';
+  }
+
+  public function getAccountEmail() {
+    return $this->getAccountID();
+  }
+
+  public function getAccountID() {
+    if ($this->accountData === null) {
+      $verify_uri = 'https://verifier.login.persona.org/verify';
+      $data = array(
+        'audience' => $this->audience,
+        'assertion' => $this->assertion,
+      );
+
+      list($body) = id(new HTTPSFuture($verify_uri, json_encode($data)))
+        ->setMethod('POST')
+        ->addHeader('Content-Type', 'application/json')
+        ->resolvex();
+
+      $response = json_decode($body, true);
+      if (!is_array($response)) {
+        throw new Exception("Unexpected Persona response: {$body}");
+      }
+
+      $audience = idx($response, 'audience');
+      if ($audience != $this->audience) {
+        throw new Exception("Mismatched Persona audience: {$audience}");
+      }
+
+      if (idx($response, 'status') !== 'okay') {
+        $reason = idx($response, 'reason', 'Unknown');
+        throw new Exception("Persona login failed: {$reason}");
+      }
+
+      $this->accountData = $response;
+    }
+
+    return idx($this->accountData, 'email');
+  }
+
+}
