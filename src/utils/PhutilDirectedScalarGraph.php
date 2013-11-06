dc6e2bdf4c3c2171a5297a2910ae284b7745c3a1
epriestley <git@epriestley.com>
2013-11-06 12:39:54 -0800

    Add "PhutilDirectedScalarGraph"
    Summary: Ref T4068. I need a non-fancy version of this to topographically sort commits.
    
    Test Plan: See next revision.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T4068
    
    Differential Revision: https://secure.phabricator.com/D7517

---
+++ src/utils/PhutilDirectedScalarGraph.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Concrete subclass of @{class:AbstractDirectedGraph} which can not load any
+ * data from external sources.
+ */
+final class PhutilDirectedScalarGraph extends AbstractDirectedGraph {
+
+  protected function loadEdges(array $nodes) {
+    throw new Exception(
+      "PhutilDirectedScalarGraph can not load additional nodes at runtime. ".
+      "Tried to load: ".implode(', ', $nodes));
+  }
+
+}
