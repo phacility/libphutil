a25afc6c64b0eabf7d4c63f0bda79efe60d37147
epriestley <git@epriestley.com>
2014-03-22 18:06:35 -0700

    Put a <span> around "NOTE:" in Remarkup
    Summary: I suspect these will look better if we bold/grey the "NOTE:" part.
    
    Test Plan: Ran unit tests. Will try styling next.
    
    Reviewers: btrahan, chad
    
    Reviewed By: chad
    
    Subscribers: epriestley
    
    Differential Revision: https://secure.phabricator.com/D8596

--- src/markup/engine/remarkup/blockrule/PhutilRemarkupEngineRemarkupNoteBlockRule.php
+++ src/markup/engine/remarkup/blockrule/PhutilRemarkupEngineRemarkupNoteBlockRule.php
@@ -29,40 +29,41 @@ final class PhutilRemarkupEngineRemarkupNoteBlockRule
   public function markupText($text) {
     $matches = array();
     preg_match($this->getRegEx(), $text, $matches);
-    switch ($matches[0]) {
-      case 'NOTE: ':
-        $class_suffix = 'note';
-        $truncate = 0;
-        break;
-      case '(NOTE) ':
-        $class_suffix = 'note';
-        $truncate = 7;
-        break;
-      case 'IMPORTANT: ':
-        $class_suffix = 'important';
-        $truncate = 0;
-        break;
-      case '(IMPORTANT) ':
-        $class_suffix = 'important';
-        $truncate = 12;
-        break;
-      case 'WARNING: ':
-        $class_suffix = 'warning';
-        $truncate = 0;
-        break;
-      case '(WARNING) ':
-        $class_suffix = 'warning';
-        $truncate = 10;
-        break;
-    }
-    $text_mode = $this->getEngine()->isTextMode();
-    if ($truncate && !$text_mode) {
-      $text = substr($text, $truncate);
-    }
-    $text = $this->applyRules(rtrim($text));
 
+    if (idx($matches, 'showword')) {
+      $word = $matches['showword'];
+      $show = true;
+    } else {
+      $word = $matches['hideword'];
+      $show = false;
+    }
+
+    $class_suffix = phutil_utf8_strtolower($word);
+
+    // This is the "(IMPORTANT)" or "NOTE:" part.
+    $word_part = rtrim(substr($text, 0, strlen($matches[0])));
+
+    // This is the actual text.
+    $text_part = substr($text, strlen($matches[0]));
+    $text_part = $this->applyRules(rtrim($text_part));
+
+    $text_mode = $this->getEngine()->isTextMode();
     if ($text_mode) {
-      return $text;
+      return $word_part.' '.$text_part;
+    }
+
+    if ($show) {
+      $content = array(
+        phutil_tag(
+          'span',
+          array(
+            'class' => 'remarkup-note-word',
+          ),
+          $word_part),
+        ' ',
+        $text_part);
+    } else {
+      $content = $text_part;
     }
 
     return phutil_tag(
@@ -70,11 +71,26 @@ final class PhutilRemarkupEngineRemarkupNoteBlockRule
       array(
         'class' => 'remarkup-'.$class_suffix
       ),
-      $text);
+      $content);
   }
 
   private function getRegEx() {
-    return "/^(NOTE: |IMPORTANT: |WARNING: |".
-           "\(NOTE\) |\(WARNING\) |\(IMPORTANT\) )/";
+    $words = array(
+      'NOTE',
+      'IMPORTANT',
+      'WARNING',
+    );
+
+    foreach ($words as $k => $word) {
+      $words[$k] = preg_quote($word, '/');
+    }
+    $words = implode('|', $words);
+
+    return
+      '/^(?:'.
+      '(?:\((?P<hideword>'.$words.')\))'.
+      '|'.
+      '(?:(?P<showword>'.$words.'):))\s*'.
+      '/';
   }
 }
