90aaeb01bb9291459fa6b6d8fedbaacab1f70cb3
Ashish SHUKLA <ashish@automattic.com>
2014-05-08 12:15:40 -0700

    Add WordPress.com OAuth2 plugin
    Summary:
    This plugin provides an OAuth authentication provider to authenticate users using WordPress.com Connect.
    
    This diff corresponds to the github pull request: https://github.com/facebook/libphutil/pull/36
    
    Test Plan: No test plan, at the moment.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: Korvin, epriestley
    
    Differential Revision: https://secure.phabricator.com/D9004

---
+++ src/future/wordpress/PhutilWordPressFuture.php
@@ -0,0 +1,87 @@
+<?php
+
+final class PhutilWordPressFuture extends FutureProxy {
+
+  private $future;
+  private $clientID;
+  private $accessToken;
+  private $action;
+  private $params;
+  private $method = 'GET';
+
+  public function __construct() {
+    parent::__construct(null);
+  }
+
+  public function setAccessToken($token) {
+    $this->accessToken = $token;
+    return $this;
+  }
+
+  public function setClientID($client_id) {
+    $this->clientID = $client_id;
+    return $this;
+  }
+
+  public function setRawWordPressQuery($action, array $params = array()) {
+    $this->action = $action;
+    $this->params = $params;
+    return $this;
+  }
+
+  public function setMethod($method) {
+    $this->method = $method;
+    return $this;
+  }
+
+  protected function getProxiedFuture() {
+    if (!$this->future) {
+      $params = $this->params;
+
+      if (!$this->action) {
+        throw new Exception("You must setRawWordPressQuery()!");
+      }
+
+      if (!$this->accessToken) {
+        throw new Exception("You must setAccessToken()!");
+      }
+
+      $uri = new PhutilURI('https://public-api.wordpress.com/');
+      $uri->setPath('/rest/v1/'.ltrim($this->action, '/'));
+
+      $future = new HTTPSFuture($uri);
+      $future->setData($this->params);
+      $future->setMethod($this->method);
+
+      // NOTE: This is how WordPress.com REST API authenticates
+      $future->addHeader('Authorization', 'Bearer ' . $this->accessToken);
+
+      $this->future = $future;
+    }
+
+    return $this->future;
+  }
+
+  protected function didReceiveResult($result) {
+    list($status, $body, $headers) = $result;
+
+    if ($status->isError()) {
+      throw $status;
+    }
+
+    $data = phutil_json_decode($body);
+    if (empty($data)) {
+      throw new Exception(
+        pht('Expected JSON response from WordPress.com, got: %s', $body));
+    }
+
+    if (idx($data, 'error')) {
+      $error = $data['error'];
+      throw new Exception(
+        pht('Received error from WordPress.com: %s', $error));
+    }
+
+    return $data;
+  }
+
+}
