phacility/libphutil

commit 48a04395363d6c1dd9f66057bd11fd70d4665ba9

Use `phutil_var_export` for `PhutilParserGenerator::formatAndIndent`
Summary: `phutil_var_export` produces more readable output than `var_export`. Depends on D9923.

Test Plan: Eye-balled the `PhutilTypeSpec` code.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: epriestley, Korvin

Differential Revision: https://secure.phabricator.com/D9925

src/parser/PhutilParserGenerator.php
@@ -776,7 +776,7 @@ final class PhutilParserGenerator {
   }
 
   private function formatAndIndent($var, $depth) {
-    $var = var_export($var, true);
+    $var = phutil_var_export($var, true);
     $var = str_replace("\n", "\n".str_repeat(' ', $depth), $var);
 
     return $var;

commit 81e885e2b5f6311582a66eb53f0701474563a08d

Minor fix for `PhutilParserGenerator`
Summary: `phutil_var_export` only accepts a single parameter. This bug was introduced in D9925.

Test Plan: Eye-ball it.

Reviewers: #blessed_reviewers, epriestley

Reviewed By: #blessed_reviewers, epriestley

Subscribers: epriestley, Korvin

Differential Revision: https://secure.phabricator.com/D9929

src/parser/PhutilParserGenerator.php
@@ -776,7 +776,7 @@ final class PhutilParserGenerator {
   }
 
   private function formatAndIndent($var, $depth) {
-    $var = phutil_var_export($var, true);
+    $var = phutil_var_export($var);
     $var = str_replace("\n", "\n".str_repeat(' ', $depth), $var);
 
     return $var;