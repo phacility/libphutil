95152847ba50bc289f81e5127b97caf52c62ad9c
epriestley <git@epriestley.com>
2014-12-12 11:29:50 -0800

    Preserve whitespace more faithfully when processing %%% remarkup blocks
    Summary:
    This improves the output for some edge cases with `%%%` blocks. Notably, blocks did not emit a block-level spacing tag, so this:
    
    ```
    %%%a%%%
    %%%b%%%
    ```
    
    ...rendered on a single line.
    
    Test Plan:
    Added unit tests.
    
    {F249620}
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    Subscribers: chasemp, epriestley
    
    Differential Revision: https://secure.phabricator.com/D10984

--- src/markup/engine/remarkup/blockrule/PhutilRemarkupLiteralBlockRule.php
+++ src/markup/engine/remarkup/blockrule/PhutilRemarkupLiteralBlockRule.php
@@ -3,8 +3,23 @@
 final class PhutilRemarkupLiteralBlockRule extends PhutilRemarkupBlockRule {
 
   public function getMatchingLineCount(array $lines, $cursor) {
+    // NOTE: We're consuming all continguous blocks of %%% literals, so this:
+    //
+    //    %%%a%%%
+    //    %%%b%%%
+    //
+    // ...is equivalent to:
+    //
+    //    %%%a
+    //    b%%%
+    //
+    // If they are separated by a blank newline, they are parsed as two
+    // different blocks. This more clearly represents the original text in the
+    // output text and assists automated escaping of blocks coming into the
+    // system.
+
     $num_lines = 0;
-    if (preg_match('/^\s*%%%/', $lines[$cursor])) {
+    while (preg_match('/^\s*%%%/', $lines[$cursor])) {
       $num_lines++;
 
       while (isset($lines[$cursor])) {
@@ -18,28 +33,50 @@ final class PhutilRemarkupLiteralBlockRule extends PhutilRemarkupBlockRule {
 
       $cursor++;
 
+      $found_empty = false;
       while (isset($lines[$cursor])) {
         if (!strlen(trim($lines[$cursor]))) {
           $num_lines++;
           $cursor++;
+          $found_empty = true;
           continue;
         }
         break;
       }
 
+      if ($found_empty) {
+        // If there's an empty line after the block, stop merging blocks.
+        break;
+      }
+
+      if (!isset($lines[$cursor])) {
+        // If we're at the end of the input, stop looking for more lines.
+        break;
+      }
     }
 
     return $num_lines;
   }
 
   public function markupText($text, $children) {
-    $text = preg_replace('/^\s*%%%(.*)%%%\s*\z/s', '\1', $text);
-    if ($this->getEngine()->isTextMode()) {
-      return $text;
+    $text = rtrim($text);
+    $text = phutil_split_lines($text, $retain_endings = true);
+    foreach ($text as $key => $line) {
+      $line = preg_replace('/^\s*%%%/', '', $line);
+      $line = preg_replace('/%%%(\s*)\z/', '\1', $line);
+      $text[$key] = $line;
     }
 
-    $text = phutil_split_lines($text, $retain_endings = true);
-    return phutil_implode_html(phutil_tag('br', array()), $text);
+    if ($this->getEngine()->isTextMode()) {
+      return implode('', $text);
+    }
+
+    return phutil_tag(
+      'p',
+      array(
+        'class' => 'remarkup-literal',
+      ),
+      phutil_implode_html(phutil_tag('br', array()), $text));
   }
 
 }
