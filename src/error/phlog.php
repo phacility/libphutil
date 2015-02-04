94ab88300c802c445d0bb57dccefd8796e61b16b
Joshua Spence <josh@joshuaspence.com>
2015-02-05 07:22:26 +1100

    Minor tidying of `phlog`
    Summary: Self-explanatory.
    
    Test Plan: Eyeball it.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: Korvin, epriestley
    
    Differential Revision: https://secure.phabricator.com/D11676

--- src/error/phlog.php
+++ src/error/phlog.php
@@ -1,19 +1,17 @@
 <?php
 
-
 /**
  * libphutil log function for development debugging. Takes any argument and
  * forwards it to registered listeners. This is essentially a more powerful
- * version of ##error_log()##.
+ * version of `error_log()`.
  *
- * @param wild Any value you want printed to the error log or other registered
- *             logs/consoles.
- * @param ...  Other values to be logged.
- * @return wild Passed $value.
+ * @param  wild  Any value you want printed to the error log or other registered
+ *               logs/consoles.
+ * @param  ...   Other values to be logged.
+ * @return wild  Passed $value.
  */
 function phlog($value/* , ... */) {
-
-  // Get the caller information
+  // Get the caller information.
   $trace = debug_backtrace();
   $metadata = array(
     'file' => $trace[0]['file'],
@@ -33,31 +31,30 @@ function phlog($value/* , ... */) {
   return $value;
 }
 
-
 /**
  * Example @{class:PhutilErrorHandler} error listener callback. When you call
- * ##PhutilErrorHandler::setErrorListener()##, you must pass a callback function
+ * `PhutilErrorHandler::setErrorListener()`, you must pass a callback function
  * with the same signature as this one.
  *
  * NOTE: @{class:PhutilErrorHandler} handles writing messages to the error
  * log, so you only need to provide a listener if you have some other console
  * (like Phabricator's DarkConsole) which you //also// want to send errors to.
  *
- * NOTE: You will receive errors which were silenced with the "@" operator. If
- * you don't want to display these, test for "@" being in effect by checking if
- * ##error_reporting() === 0## before displaying the error.
+ * NOTE: You will receive errors which were silenced with the `@` operator. If
+ * you don't want to display these, test for `@` being in effect by checking if
+ * `error_reporting() === 0` before displaying the error.
  *
- * @param const A PhutilErrorHandler constant, like PhutilErrorHandler::ERROR,
- *              which indicates the event type (e.g. error, exception,
- *              user message).
- * @param wild  The event value, like the Exception object for an exception
- *              event, an error string for an error event, or some user object
- *              for user messages.
- * @param dict  A dictionary of metadata about the event. The keys 'file',
- *              'line' and 'trace' are always available. Other keys may be
- *              present, depending on the event type.
+ * @param  const  A PhutilErrorHandler constant, like PhutilErrorHandler::ERROR,
+ *                which indicates the event type (e.g. error, exception,
+ *                user message).
+ * @param  wild   The event value, like the Exception object for an exception
+ *                event, an error string for an error event, or some user object
+ *                for user messages.
+ * @param  dict   A dictionary of metadata about the event. The keys 'file',
+ *                'line' and 'trace' are always available. Other keys may be
+ *                present, depending on the event type.
  * @return void
  */
 function phutil_error_listener_example($event, $value, array $metadata) {
-  throw new Exception('This is just an example function!');
+  throw new Exception(pht('This is just an example function!'));
 }
