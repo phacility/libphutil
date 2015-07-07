a0ea87feb8eb95b9bca0fd6ac10f7dc0602b468c
epriestley <git@epriestley.com>
2015-07-07 13:35:03 -0700

    Remove referneces to "www.phabricator.com" in libphutil
    Summary: This host is out of service.
    
    Test Plan: `git grep -i www.phabricator.com`, triggered exception, read text.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Subscribers: epriestley
    
    Differential Revision: https://secure.phabricator.com/D13587

--- src/symbols/exception/PhutilMissingSymbolException.php
+++ src/symbols/exception/PhutilMissingSymbolException.php
@@ -5,16 +5,22 @@ final class PhutilMissingSymbolException extends Exception {
   public function __construct($symbol, $type, $reason) {
     parent::__construct(
       pht(
-        "Failed to load %s '%s': %s ".
-        "If this symbol was recently added or moved, your library map may ".
-        "be out of date. You can rebuild the map by running '%s'. ".
-        "For more information, see: %s",
+        "Failed to load %s '%s': %s.\n\n".
+        "If you are not a developer, this almost always means that a library ".
+        "is out of date. For example, you may have upgraded `phabricator` ".
+        "without upgrading `libphutil`, or vice versa. It might also mean ".
+        "that you need to restart Apache or PHP-FPM. Make sure all libraries ".
+        "are up to date and all services have been restarted.\n\n".
+        "If you are a developer and this symbol was recently added or moved, ".
+        "your library map may need to be rebuilt. You can rebuild the map by ".
+        "running '%s'. For more information, see:\n\n".
+        "%s",
         $type,
         $symbol,
         $reason,
         'arc liberate',
-        'http://www.phabricator.com/docs/phabricator/article/'.
-        'libphutil_Libraries_User_Guide.html'));
+        'https://secure.phabricator.com/book/phabcontrib/article/'.
+          'adding_new_classes/'));
   }
 
 }
