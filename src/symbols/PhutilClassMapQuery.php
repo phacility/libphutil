44bbc7be7b5c2646fa23780b10c50e919fd7a9c0
Joshua Spence <josh@joshuaspence.com>
2015-07-08 20:02:43 +1000

    Add filter method to PhutilClassMapQuery
    Summary: Allow `PhutilClassMapQuery` to easily filter objects from the map.
    
    Test Plan: See next diff.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D13588

--- src/symbols/PhutilClassMapQuery.php
+++ src/symbols/PhutilClassMapQuery.php
@@ -41,6 +41,7 @@ final class PhutilClassMapQuery extends Phobject {
 
   private $ancestorClass;
   private $expandMethod;
+  private $filterMethod;
   private $filterNull = false;
   private $uniqueMethod;
   private $sortMethod;
@@ -148,6 +149,19 @@ final class PhutilClassMapQuery extends Phobject {
   }
 
 
+  /**
+   * Provide a method to filter the map.
+   *
+   * @param string Name of the filtering method.
+   * @return this
+   * @task config
+   */
+  public function setFilterMethod($filter_method) {
+    $this->filterMethod = $filter_method;
+    return $this;
+  }
+
+
 /* -(  Executing the Query  )------------------------------------------------ */
 
 
@@ -191,6 +205,7 @@ final class PhutilClassMapQuery extends Phobject {
     }
 
     $expand = $this->expandMethod;
+    $filter = $this->filterMethod;
     $unique = $this->uniqueMethod;
     $sort = $this->sortMethod;
 
@@ -251,6 +266,11 @@ final class PhutilClassMapQuery extends Phobject {
       }
     } else {
       $map = $list;
+    }
+
+    // Apply the "filter" mechanism, if it is configured.
+    if (strlen($filter)) {
+      $map = mfilter($map, $filter);
     }
 
     // Apply the "sort" mechanism, if it is configured.
