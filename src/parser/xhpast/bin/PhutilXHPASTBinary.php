phacility/libphutil

commit e8eaafe04d640924d47ff9ed72984f1b0630d808

Improve parsing of `use` statements
Summary:
The way that we parse `use` statements at the moment is a bit odd. For example, consider the following example:

```lang=php
<?php

use A\B\C;
use X\Y\Z;
```

This is currently parsed as:

{F976619, size=full}

After this change, it is instead parsed as:

{F976621, size=full}

Test Plan: Added a test case.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: Korvin

Differential Revision: https://secure.phabricator.com/D14518

src/parser/xhpast/bin/PhutilXHPASTBinary.php
@@ -8,7 +8,7 @@ final class PhutilXHPASTBinary extends Phobject {
    * This is the version that would be obtained with an up-to-date XHPAST
    * build. The //actual// XHPAST build version may vary.
    */
-  const EXPECTED_VERSION = '5.8.1';
+  const EXPECTED_VERSION = '5.8.2';
 
   /**
    * The XHPAST build version.

support/xhpast/parser.y
@@ -258,7 +258,7 @@ top_statement:
   $$ = NNEW(n_STATEMENT)->appendChild($1);
   }
 | T_USE use_declarations ';' {
-  NSPAN($1, n_USE, $2);
+  NSPAN($1, n_USE_LIST, $2);
   $1->appendChild($2);
   $$ = NNEW(n_STATEMENT)->appendChild($1);
   NMORE($$, $3);
@@ -274,8 +274,7 @@ use_declarations:
     $$ = $1->appendChild($3);
   }
 | use_declaration {
-    $$ = NNEW(n_USE_LIST);
-    $$->appendChild($1);
+    NMORE($$, $1);
   }
 ;
 

support/xhpast/xhpast.cpp
@@ -12,7 +12,7 @@ void print_node(xhpast::Node *node);
 int main(int argc, char* argv[]) {
   if (argc != 1) {
     // Coupling: modify also src/parser/xhpast/bin/PhutilXHPASTBinary.php
-    cout << "5.8.1\n";
+    cout << "5.8.2\n";
     return 0;
   }
 

src/parser/xhpast/__tests__/data/php-syntax.php.test
@@ -2064,29 +2064,22 @@ pass
             410,
             [
               [
-                9010,
+                9009,
                 407,
                 409,
                 [
                   [
-                    9009,
+                    9010,
                     409,
                     409,
                     [
                       [
-                        9010,
+                        9001,
                         409,
-                        409,
-                        [
-                          [
-                            9001,
-                            409,
-                            409
-                          ],
-                          [
-                            9005
-                          ]
-                        ]
+                        409
+                      ],
+                      [
+                        9005
                       ]
                     ]
                   ]
@@ -2100,29 +2093,22 @@ pass
             418,
             [
               [
-                9010,
+                9009,
                 412,
                 417,
                 [
                   [
-                    9009,
+                    9010,
                     414,
                     417,
                     [
                       [
-                        9010,
+                        9001,
                         414,
-                        414,
-                        [
-                          [
-                            9001,
-                            414,
-                            414
-                          ],
-                          [
-                            9005
-                          ]
-                        ]
+                        414
+                      ],
+                      [
+                        9005
                       ],
                       [
                         9010,
@@ -2151,31 +2137,24 @@ pass
             446,
             [
               [
-                9010,
+                9009,
                 420,
                 445,
                 [
                   [
-                    9009,
+                    9010,
                     422,
                     445,
                     [
                       [
-                        9010,
+                        9001,
                         422,
+                        422
+                      ],
+                      [
+                        9013,
                         426,
-                        [
-                          [
-                            9001,
-                            422,
-                            422
-                          ],
-                          [
-                            9013,
-                            426,
-                            426
-                          ]
-                        ]
+                        426
                       ],
                       [
                         9010,

src/parser/xhpast/__tests__/data/use.php.test
@@ -1,0 +1,380 @@
+<?php
+
+use A;
+use \B;
+use C as D;
+use E, F;
+use G, H as I;
+~~~~~~~~~~
+pass
+~~~~~~~~~~
+{
+  "tree": [
+    9000,
+    0,
+    41,
+    [
+      [
+        9006,
+        0,
+        40,
+        [
+          [
+            9007,
+            0,
+            0
+          ],
+          [
+            9004,
+            2,
+            5,
+            [
+              [
+                9009,
+                2,
+                4,
+                [
+                  [
+                    9010,
+                    4,
+                    4,
+                    [
+                      [
+                        9001,
+                        4,
+                        4
+                      ],
+                      [
+                        9005
+                      ]
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ],
+          [
+            9004,
+            7,
+            11,
+            [
+              [
+                9009,
+                7,
+                10,
+                [
+                  [
+                    9010,
+                    9,
+                    10,
+                    [
+                      [
+                        9001,
+                        9,
+                        10
+                      ],
+                      [
+                        9005
+                      ]
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ],
+          [
+            9004,
+            13,
+            20,
+            [
+              [
+                9009,
+                13,
+                19,
+                [
+                  [
+                    9010,
+                    15,
+                    19,
+                    [
+                      [
+                        9001,
+                        15,
+                        15
+                      ],
+                      [
+                        9013,
+                        19,
+                        19
+                      ]
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ],
+          [
+            9004,
+            22,
+            28,
+            [
+              [
+                9009,
+                22,
+                27,
+                [
+                  [
+                    9010,
+                    24,
+                    27,
+                    [
+                      [
+                        9001,
+                        24,
+                        24
+                      ],
+                      [
+                        9005
+                      ],
+                      [
+                        9010,
+                        27,
+                        27,
+                        [
+                          [
+                            9001,
+                            27,
+                            27
+                          ],
+                          [
+                            9005
+                          ]
+                        ]
+                      ]
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ],
+          [
+            9004,
+            30,
+            40,
+            [
+              [
+                9009,
+                30,
+                39,
+                [
+                  [
+                    9010,
+                    32,
+                    39,
+                    [
+                      [
+                        9001,
+                        32,
+                        32
+                      ],
+                      [
+                        9005
+                      ],
+                      [
+                        9010,
+                        35,
+                        39,
+                        [
+                          [
+                            9001,
+                            35,
+                            35
+                          ],
+                          [
+                            9013,
+                            39,
+                            39
+                          ]
+                        ]
+                      ]
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    ]
+  ],
+  "stream": [
+    [
+      371,
+      5
+    ],
+    [
+      375,
+      2
+    ],
+    [
+      343,
+      3
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      59,
+      1
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      343,
+      3
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      387,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      59,
+      1
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      343,
+      3
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      329,
+      2
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      59,
+      1
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      343,
+      3
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      44,
+      1
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      59,
+      1
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      343,
+      3
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      44,
+      1
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      329,
+      2
+    ],
+    [
+      375,
+      1
+    ],
+    [
+      309,
+      1
+    ],
+    [
+      59,
+      1
+    ],
+    [
+      375,
+      1
+    ]
+  ]
+}

commit f0881b37049c7ac08b4a8c04c6c7cd31c70b4942

Fix an issue with parsing `n_USE_LIST`
Summary: Fix a minor issue with parsing `n_USE_LIST` nodes caused by D14518.

Test Plan:
| {F1001821} | {F1001822} |
| **Before** | **After** |

Reviewers: #blessed_reviewers, epriestley

Reviewed By: #blessed_reviewers, epriestley

Subscribers: Korvin

Differential Revision: https://secure.phabricator.com/D14570

src/parser/xhpast/bin/PhutilXHPASTBinary.php
@@ -8,7 +8,7 @@ final class PhutilXHPASTBinary extends Phobject {
    * This is the version that would be obtained with an up-to-date XHPAST
    * build. The //actual// XHPAST build version may vary.
    */
-  const EXPECTED_VERSION = '5.8.2';
+  const EXPECTED_VERSION = '5.8.3';
 
   /**
    * The XHPAST build version.

support/xhpast/parser.y
@@ -258,9 +258,8 @@ top_statement:
   $$ = NNEW(n_STATEMENT)->appendChild($1);
   }
 | T_USE use_declarations ';' {
-  NSPAN($1, n_USE_LIST, $2);
-  $1->appendChild($2);
-  $$ = NNEW(n_STATEMENT)->appendChild($1);
+  NMORE($2, $1);
+  $$ = NNEW(n_STATEMENT)->appendChild($2);
   NMORE($$, $3);
   }
 | constant_declaration ';' {
@@ -274,7 +273,8 @@ use_declarations:
     $$ = $1->appendChild($3);
   }
 | use_declaration {
-    NMORE($$, $1);
+    $$ = NNEW(n_USE_LIST);
+    $$->appendChild($1);
   }
 ;
 

support/xhpast/parser.yacc.cpp
@@ -624,7 +624,7 @@ static const yytype_uint8 yytranslate[] =
 static const yytype_uint16 yyrline[] =
 {
        0,   207,   207,   213,   216,   222,   225,   231,   232,   233,
-     234,   239,   246,   252,   260,   266,   273,   276,   282,   287,
+     234,   239,   246,   252,   260,   265,   272,   275,   282,   287,
      293,   299,   309,   316,   326,   329,   335,   336,   337,   338,
      346,   347,   353,   356,   359,   365,   368,   398,   417,   424,
      432,   445,   452,   459,   466,   473,   480,   487,   494,   501,
@@ -3563,35 +3563,35 @@ yyreduce:
   case 14:
 #line 260 "parser.y" /* yacc.c:1646  */
     {
-  NSPAN((yyvsp[-2]), n_USE_LIST, (yyvsp[-1]));
-  (yyvsp[-2])->appendChild((yyvsp[-1]));
-  (yyval) = NNEW(n_STATEMENT)->appendChild((yyvsp[-2]));
+  NMORE((yyvsp[-1]), (yyvsp[-2]));
+  (yyval) = NNEW(n_STATEMENT)->appendChild((yyvsp[-1]));
   NMORE((yyval), (yyvsp[0]));
   }
-#line 3572 "parser.yacc.cpp" /* yacc.c:1646  */
+#line 3571 "parser.yacc.cpp" /* yacc.c:1646  */
     break;
 
   case 15:
-#line 266 "parser.y" /* yacc.c:1646  */
+#line 265 "parser.y" /* yacc.c:1646  */
     {
   (yyval) = NNEW(n_STATEMENT)->appendChild((yyvsp[-1]));
   NMORE((yyval), (yyvsp[0]));
   }
-#line 3581 "parser.yacc.cpp" /* yacc.c:1646  */
+#line 3580 "parser.yacc.cpp" /* yacc.c:1646  */
     break;
 
   case 16:
-#line 273 "parser.y" /* yacc.c:1646  */
+#line 272 "parser.y" /* yacc.c:1646  */
     {
     (yyval) = (yyvsp[-2])->appendChild((yyvsp[0]));
   }
-#line 3589 "parser.yacc.cpp" /* yacc.c:1646  */
+#line 3588 "parser.yacc.cpp" /* yacc.c:1646  */
     break;
 
   case 17:
-#line 276 "parser.y" /* yacc.c:1646  */
+#line 275 "parser.y" /* yacc.c:1646  */
     {
-    NMORE((yyval), (yyvsp[0]));
+    (yyval) = NNEW(n_USE_LIST);
+    (yyval)->appendChild((yyvsp[0]));
   }
 #line 3597 "parser.yacc.cpp" /* yacc.c:1646  */
     break;

support/xhpast/xhpast.cpp
@@ -12,7 +12,7 @@ void print_node(xhpast::Node *node);
 int main(int argc, char* argv[]) {
   if (argc != 1) {
     // Coupling: modify also src/parser/xhpast/bin/PhutilXHPASTBinary.php
-    cout << "5.8.2\n";
+    cout << "5.8.3\n";
     return 0;
   }
 

src/parser/xhpast/__tests__/data/php-syntax.php.test
@@ -2100,7 +2100,7 @@ pass
                   [
                     9010,
                     414,
-                    417,
+                    414,
                     [
                       [
                         9001,
@@ -2109,21 +2109,21 @@ pass
                       ],
                       [
                         9005
+                      ]
+                    ]
+                  ],
+                  [
+                    9010,
+                    417,
+                    417,
+                    [
+                      [
+                        9001,
+                        417,
+                        417
                       ],
                       [
-                        9010,
-                        417,
-                        417,
-                        [
-                          [
-                            9001,
-                            417,
-                            417
-                          ],
-                          [
-                            9005
-                          ]
-                        ]
+                        9005
                       ]
                     ]
                   ]
@@ -2144,7 +2144,7 @@ pass
                   [
                     9010,
                     422,
-                    445,
+                    426,
                     [
                       [
                         9001,
@@ -2155,55 +2155,55 @@ pass
                         9013,
                         426,
                         426
-                      ],
+                      ]
+                    ]
+                  ],
+                  [
+                    9010,
+                    429,
+                    433,
+                    [
                       [
-                        9010,
+                        9001,
                         429,
+                        429
+                      ],
+                      [
+                        9013,
                         433,
-                        [
-                          [
-                            9001,
-                            429,
-                            429
-                          ],
-                          [
-                            9013,
-                            433,
-                            433
-                          ]
-                        ]
-                      ],
+                        433
+                      ]
+                    ]
+                  ],
+                  [
+                    9010,
+                    436,
+                    437,
+                    [
                       [
-                        9010,
+                        9001,
                         436,
-                        437,
-                        [
-                          [
-                            9001,
-                            436,
-                            437
-                          ],
-                          [
-                            9005
-                          ]
-                        ]
+                        437
                       ],
                       [
-                        9010,
+                        9005
+                      ]
+                    ]
+                  ],
+                  [
+                    9010,
+                    440,
+                    445,
+                    [
+                      [
+                        9001,
                         440,
+                        441
+                      ],
+                      [
+                        9013,
                         445,
-                        [
-                          [
-                            9001,
-                            440,
-                            441
-                          ],
-                          [
-                            9013,
-                            445,
-                            445
-                          ]
-                        ]
+                        445
                       ]
                     ]
                   ]

src/parser/xhpast/__tests__/data/use.php.test
@@ -126,7 +126,7 @@ pass
                   [
                     9010,
                     24,
-                    27,
+                    24,
                     [
                       [
                         9001,
@@ -135,21 +135,21 @@ pass
                       ],
                       [
                         9005
+                      ]
+                    ]
+                  ],
+                  [
+                    9010,
+                    27,
+                    27,
+                    [
+                      [
+                        9001,
+                        27,
+                        27
                       ],
                       [
-                        9010,
-                        27,
-                        27,
-                        [
-                          [
-                            9001,
-                            27,
-                            27
-                          ],
-                          [
-                            9005
-                          ]
-                        ]
+                        9005
                       ]
                     ]
                   ]
@@ -170,7 +170,7 @@ pass
                   [
                     9010,
                     32,
-                    39,
+                    32,
                     [
                       [
                         9001,
@@ -179,23 +179,23 @@ pass
                       ],
                       [
                         9005
+                      ]
+                    ]
+                  ],
+                  [
+                    9010,
+                    35,
+                    39,
+                    [
+                      [
+                        9001,
+                        35,
+                        35
                       ],
                       [
-                        9010,
-                        35,
+                        9013,
                         39,
-                        [
-                          [
-                            9001,
-                            35,
-                            35
-                          ],
-                          [
-                            9013,
-                            39,
-                            39
-                          ]
-                        ]
+                        39
                       ]
                     ]
                   ]