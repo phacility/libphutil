c3fd3a8bb2c319982279ead972354c8bf760145c
epriestley <git@epriestley.com>
2016-01-14 14:40:54 -0800

    fix bug
    Summary: big bug
    
    Test Plan: !!!!!
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Differential Revision: https://secure.phabricator.com/D15025

--- src/markup/engine/remarkup/markuprule/PhutilRemarkupHighlightRule.php
+++ src/markup/engine/remarkup/markuprule/PhutilRemarkupHighlightRule.php
@@ -21,6 +21,13 @@ final class PhutilRemarkupHighlightRule extends PhutilRemarkupRule {
     // Remove the two exclamation points that represent syntax.
     $excitement = substr($matches[2], 2);
 
+    // If the internal content consists of ONLY exclamation points, leave it
+    // untouched so "!!!!!" is five exclamation points instead of one
+    // highlighted exclamation point.
+    if (preg_match('/^!+\z/', $matches[1])) {
+      return $matches[0];
+    }
+
     // $excitement now has two fewer !'s than we started with.
     return hsprintf('<span class="remarkup-highlight">%s%s</span>',
       $matches[1], $excitement);
