7a184448aff55465476da0e13477b7e6f3446a14
Chad Little <chad@phacility.com>
2016-04-14 15:29:17 -0700

    Add Spanish (Spain) locale to libphutil
    Summary: Adds the rough-in for Spanish Locales. Fixes T10814.
    
    Test Plan: Visit settings, choose new locale
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin
    
    Maniphest Tasks: T10814
    
    Differential Revision: https://secure.phabricator.com/D15718

---
+++ src/internationalization/locales/PhutilSpanishSpainLocale.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The Spanish (Spain) Locale
+ */
+final class PhutilSpanishSpainLocale extends PhutilLocale {
+
+  public function getLocaleCode() {
+    return 'es_ES';
+  }
+
+  public function getLocaleName() {
+    return pht('Spanish (Spain)');
+  }
+
+}
