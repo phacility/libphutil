b00eff539ee72e8dcab49028f197f38c01092d44
Chad <chad@dev.legacygt.com>
2016-09-06 15:51:46 +0000

    Add Slack authentication adapter
    Summary: This adds an authentication adapter to libphutil for Slack. Diff 1 of 2.
    
    Test Plan: Create a new auth provider for Slack. Link Slack account to phacility.slack.com. Authenticate a new user, create an account with Slack reg provider.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin
    
    Differential Revision: https://secure.phabricator.com/D16495

---
+++ src/future/slack/PhutilSlackFuture.php
@@ -0,0 +1,87 @@
+<?php
+
+final class PhutilSlackFuture extends FutureProxy {
+
+  private $future;
+  private $clientID;
+  private $accessToken;
+  private $action;
+  private $params;
+  private $method = 'GET';
+
+  public function __construct() {
+    parent::__construct(null);
+  }
+
+  public function setAccessToken($token) {
+    $this->accessToken = $token;
+    return $this;
+  }
+
+  public function setClientID($client_id) {
+    $this->clientID = $client_id;
+    return $this;
+  }
+
+  public function setRawSlackQuery($action, array $params = array()) {
+    $this->action = $action;
+    $this->params = $params;
+    return $this;
+  }
+
+  public function setMethod($method) {
+    $this->method = $method;
+    return $this;
+  }
+
+  protected function getProxiedFuture() {
+    if (!$this->future) {
+      $params = $this->params;
+
+      if (!$this->action) {
+        throw new Exception(pht('You must %s!', 'setRawSlackQuery()'));
+      }
+
+      if (!$this->accessToken) {
+        throw new Exception(pht('You must %s!', 'setAccessToken()'));
+      }
+
+      $uri = new PhutilURI('https://slack.com/');
+      $uri->setPath('/api/'.$this->action);
+      $uri->setQueryParam('token', $this->accessToken);
+
+      $future = new HTTPSFuture($uri);
+      $future->setData($this->params);
+      $future->setMethod($this->method);
+
+      $this->future = $future;
+    }
+
+    return $this->future;
+  }
+
+  protected function didReceiveResult($result) {
+    list($status, $body, $headers) = $result;
+
+    if ($status->isError()) {
+      throw $status;
+    }
+
+    $data = null;
+    try {
+      $data = phutil_json_decode($body);
+    } catch (PhutilJSONParserException $ex) {
+      throw new PhutilProxyException(
+        pht('Expected JSON response from Slack.'),
+        $ex);
+    }
+
+    if (idx($data, 'error')) {
+      $error = $data['error'];
+      throw new Exception(pht('Received error from Slack: %s', $error));
+    }
+
+    return $data;
+  }
+
+}
