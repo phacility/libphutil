6296b5d0f1e960274c722e84d575afdcb874cd61
epriestley <git@epriestley.com>
2016-12-02 03:53:17 -0800

    Implement basic IPv6 address parsing support
    Summary: Ref T11939. I'll hold this until after the release cut. We still need CIDR support (although that looks straightforward) and then testing, which should be only slightly painful since AWS supports IPv6 with some caveats now.
    
    Test Plan: Added unit tests; ran unit tests.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T11939
    
    Differential Revision: https://secure.phabricator.com/D16973

--- src/ip/__tests__/PhutilIPAddressTestCase.php
+++ src/ip/__tests__/PhutilIPAddressTestCase.php
@@ -2,7 +2,7 @@
 
 final class PhutilIPAddressTestCase extends PhutilTestCase {
 
-  public function testValidIPAddresses() {
+  public function testValidIPv4Addresses() {
     $cases = array(
       // Valid IP.
       '1.2.3.4' => true,
@@ -36,11 +36,52 @@ final class PhutilIPAddressTestCase extends PhutilTestCase {
       $this->assertEqual(
         $expect,
         !($caught instanceof Exception),
-        'PhutilIPAddress['.$input.']');
+        'PhutilIPv4Address['.$input.']');
     }
   }
 
-  public function testIPAddressToBits() {
+  public function testValidIPv6Addresses() {
+    $cases = array(
+      '::' => true,
+      '::1' => true,
+      '1::' => true,
+      '1::1' => true,
+      '1:2:3:4:5:6:7:8' => true,
+      '1:2:3::5:6:7:8' => true,
+      '1:2:3::6:7:8' => true,
+
+      // No nonsense.
+      'quack:duck' => false,
+      '11111:22222::' => false,
+
+
+      // Too long.
+      '1:2:3:4:5:6:7:8:9' => false,
+
+      // Too short.
+      '1:2:3' => false,
+
+      // Too many omitted segments.
+      '1:2:3:::7:8:9' => false,
+      '1::3::7:8:9' => false,
+    );
+
+    foreach ($cases as $input => $expect) {
+      $caught = null;
+      try {
+        PhutilIPAddress::newAddress($input);
+      } catch (Exception $ex) {
+        $caught = $ex;
+      }
+
+      $this->assertEqual(
+        $expect,
+        !($caught instanceof Exception),
+        'PhutilIPv6Address['.$input.']');
+    }
+  }
+
+  public function testIPv4AddressToBits() {
     $cases = array(
       '0.0.0.0'         => '00000000000000000000000000000000',
       '255.255.255.255' => '11111111111111111111111111111111',
@@ -55,7 +96,89 @@ final class PhutilIPAddressTestCase extends PhutilTestCase {
       $this->assertEqual(
         $expect,
         $actual,
-        'PhutilIPAddress['.$input.']->toBits()');
+        'PhutilIPv4Address['.$input.']->toBits()');
+    }
+  }
+
+  public function testIPv6AddressToBits() {
+    $cases = array(
+      '::' =>
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000',
+      '::1' =>
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000001',
+      '1::' =>
+        '0000000000000001 0000000000000000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000',
+      '::ffff:c000:0280' =>
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 1111111111111111'.
+        PhutilIPAddress::newAddress('192.0.2.128')->toBits(),
+      '21DA:00D3:0000:2F3B:02AA:00FF:FE28:9C5A' =>
+        '0010000111011010 0000000011010011'.
+        '0000000000000000 0010111100111011'.
+        '0000001010101010 0000000011111111'.
+        '1111111000101000 1001110001011010',
+      '2001:db8::1' =>
+        '0010000000000001 0000110110111000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000000'.
+        '0000000000000000 0000000000000001',
+
+    );
+
+    foreach ($cases as $input => $expect) {
+      // Remove any spaces, these are just to make the tests above easier to
+      // read.
+      $expect = str_replace(' ', '', $expect);
+
+      $actual = PhutilIPAddress::newAddress($input)->toBits();
+      $this->assertEqual(
+        $expect,
+        $actual,
+        'PhutilIPv6Address['.$input.']->toBits()');
+    }
+  }
+
+  public function testIPv6AddressToAddress() {
+    $cases = array(
+      '::' => '::',
+      '::1' => '::1',
+      '::01' => '::1',
+      '0::0001' => '::1',
+      '0000::0001' => '::1',
+      '0000:0000::001' => '::1',
+
+      '1::' => '1::',
+      '01::' => '1::',
+      '01::0' => '1::',
+      '0001::0000' => '1::',
+
+      '1:0::0:2' => '1::2',
+      '1::0:2' => '1::2',
+      '1:0::2' => '1::2',
+
+      'CAFE::' => 'cafe::',
+      '0000:aBe:0:0:1::' => '0:abe:0:0:1::',
+
+      '1:0:0:0:2:0:0:0' => '1::2:0:0:0',
+      '1:0:0:2:0:0:0:0' => '1:0:0:2::',
+    );
+
+    foreach ($cases as $input => $expect) {
+      $actual = PhutilIPAddress::newAddress($input)->getAddress();
+      $this->assertEqual(
+        $expect,
+        $actual,
+        'PhutilIPv6Address['.$input.']->getAddress()');
     }
   }
 
