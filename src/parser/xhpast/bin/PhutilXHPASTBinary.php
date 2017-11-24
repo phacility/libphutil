phacility/libphutil

commit 2596aecadc537a69fd992bfd080449df14471831

Add support for nullable return types
Summary: Ref T4334. Adds support for nullable return type, which are a thing as of PHP 7.1.

Test Plan: Updated test cases.

Reviewers: epriestley, richardvanvelzen, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: thaiphv, wjiang, Korvin, Firehed

Maniphest Tasks: T4334

Differential Revision: https://secure.phabricator.com/D18621

src/parser/xhpast/bin/PhutilXHPASTBinary.php
@@ -8,7 +8,7 @@ final class PhutilXHPASTBinary extends Phobject {
    * This is the version that would be obtained with an up-to-date XHPAST
    * build. The //actual// XHPAST build version may vary.
    */
-  const EXPECTED_VERSION = '7.0.3';
+  const EXPECTED_VERSION = '7.1.0';
 
   /**
    * The XHPAST build version.

support/xhpast/parser.y
@@ -1085,8 +1085,15 @@ return_type:
   %empty {
     $$ = NNEW(n_EMPTY);
   }
+| ':' '?' type {
+    $$ = NNEW(n_DECLARATION_RETURN);
+    $$->appendChild($2);
+    $$->appendChild($3);
+  }
 | ':' type {
-    $$ = $2;
+    $$ = NNEW(n_DECLARATION_RETURN);
+    $$->appendChild(NNEW(n_EMPTY));
+    $$->appendChild($2);
   }
 ;
 

support/xhpast/xhpast.cpp
@@ -12,7 +12,7 @@ void print_node(xhpast::Node *node);
 int main(int argc, char* argv[]) {
   if (argc != 1) {
     // Coupling: modify also src/parser/xhpast/bin/PhutilXHPASTBinary.php
-    cout << "7.0.3\n";
+    cout << "7.1.0\n";
     return 0;
   }
 

support/xhpast/generate_nodes.php
@@ -120,6 +120,7 @@ $nodes = array(
   'n_YIELD',
   'n_FINALLY',
   'n_UNPACK',
+  'n_DECLARATION_RETURN',
 );
 
 $nodes = array_combine(

support/xhpast/parser_nodes.php
@@ -121,5 +121,6 @@ function xhp_parser_node_constants() {
     9112 => 'n_YIELD',
     9113 => 'n_FINALLY',
     9114 => 'n_UNPACK',
+    9115 => 'n_DECLARATION_RETURN',
   );
 }

commit ae80e72461d2be8968533be835a1f737de55ac42

Fix node for nullable return value
Summary: Ref T4334. Fixes a bug introduced by D18621 which created a node without a valid type.

Test Plan: N/A

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: Korvin, Firehed, wjiang

Maniphest Tasks: T4334

Differential Revision: https://secure.phabricator.com/D18639

src/parser/xhpast/bin/PhutilXHPASTBinary.php
@@ -8,7 +8,7 @@ final class PhutilXHPASTBinary extends Phobject {
    * This is the version that would be obtained with an up-to-date XHPAST
    * build. The //actual// XHPAST build version may vary.
    */
-  const EXPECTED_VERSION = '7.1.0';
+  const EXPECTED_VERSION = '7.1.1';
 
   /**
    * The XHPAST build version.

support/xhpast/parser.y
@@ -1087,7 +1087,7 @@ return_type:
   }
 | ':' '?' type {
     $$ = NNEW(n_DECLARATION_RETURN);
-    $$->appendChild($2);
+    $$->appendChild(NTYPE($2, n_STRING));
     $$->appendChild($3);
   }
 | ':' type {

support/xhpast/parser.yacc.cpp
@@ -4725,7 +4725,7 @@ yyreduce:
 #line 1088 "parser.y" /* yacc.c:1646  */
     {
     (yyval) = NNEW(n_DECLARATION_RETURN);
-    (yyval)->appendChild((yyvsp[-1]));
+    (yyval)->appendChild(NTYPE((yyvsp[-1]), n_STRING));
     (yyval)->appendChild((yyvsp[0]));
   }
 #line 4732 "parser.yacc.cpp" /* yacc.c:1646  */

support/xhpast/xhpast.cpp
@@ -12,7 +12,7 @@ void print_node(xhpast::Node *node);
 int main(int argc, char* argv[]) {
   if (argc != 1) {
     // Coupling: modify also src/parser/xhpast/bin/PhutilXHPASTBinary.php
-    cout << "7.1.0\n";
+    cout << "7.1.1\n";
     return 0;
   }
 

src/parser/xhpast/__tests__/data/return-type.php.test
@@ -246,7 +246,7 @@ pass
                     46,
                     [
                       [
-                        0,
+                        9013,
                         45,
                         45
                       ],
@@ -354,7 +354,7 @@ pass
                     72,
                     [
                       [
-                        0,
+                        9013,
                         71,
                         71
                       ],
@@ -798,7 +798,7 @@ pass
                                     170,
                                     [
                                       [
-                                        0,
+                                        9013,
                                         169,
                                         169
                                       ],
@@ -1090,7 +1090,7 @@ pass
                                     239,
                                     [
                                       [
-                                        0,
+                                        9013,
                                         238,
                                         238
                                       ],
@@ -2231,4 +2231,4 @@ pass
       2
     ]
   ]
-}
+ }