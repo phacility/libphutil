f91ef416f11fa22f939f6c2211949669422983f0
epriestley <git@epriestley.com>
2018-02-08 08:43:07 -0800

    Let PhutilEmailAddress encode addresses with complex display names somewhat properly
    Summary:
    Ref T13053. This is intended to replace the incomplete fix in D17831, which stopped some bleeding but is not correct for all inputs.
    
    This mostly appears to implement the rules in RFC2822, which don't seem exceptionally complex from the escaping side, even though it is well established that no computer can ever successfully parse an RFC2822 address.
    
    Test Plan: Added unit tests; ran unit tests.
    
    Maniphest Tasks: T13053
    
    Differential Revision: https://secure.phabricator.com/D19026

--- src/parser/PhutilEmailAddress.php
+++ src/parser/PhutilEmailAddress.php
@@ -12,7 +12,7 @@ final class PhutilEmailAddress extends Phobject {
   private $localPart;
   private $domainName;
 
-  public function __construct($email_address) {
+  public function __construct($email_address = null) {
     $email_address = trim($email_address);
 
     $matches = null;
@@ -41,8 +41,9 @@ final class PhutilEmailAddress extends Phobject {
 
   public function __toString() {
     $address = $this->getAddress();
-    if ($this->displayName) {
-      return $this->displayName.' <'.$address.'>';
+    if (strlen($this->displayName)) {
+      $display_name = $this->encodeDisplayName($this->displayName);
+      return $display_name.' <'.$address.'>';
     } else {
       return $address;
     }
@@ -75,12 +76,39 @@ final class PhutilEmailAddress extends Phobject {
     return $this->domainName;
   }
 
+  public function setAddress($address) {
+    $parts = explode('@', $address, 2);
+
+    $this->localPart = $parts[0];
+    if (isset($parts[1])) {
+      $this->domainName = $parts[1];
+    }
+
+    return $this;
+  }
+
   public function getAddress() {
     $address = $this->localPart;
-    if ($this->domainName) {
+    if (strlen($this->domainName)) {
       $address .= '@'.$this->domainName;
     }
     return $address;
+  }
+
+  private function encodeDisplayName($name) {
+    // NOTE: This is a reasonable effort based on a cursory reading of
+    // RFC2822, but may be significantly misguided.
+
+    // Newlines are not permitted, even when escaped. Discard them.
+    $name = preg_replace("/\s*[\r\n]+\s*/", ' ', $name);
+
+    // Escape double quotes and backslashes.
+    $name = addcslashes($name, '\\"');
+
+    // Quote the string.
+    $name = '"'.$name.'"';
+
+    return $name;
   }
 
 }
