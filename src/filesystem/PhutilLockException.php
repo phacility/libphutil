a0b90a12cee9f2397bfc6ad07ca0c4bb66fd5d71
epriestley <git@epriestley.com>
2018-10-01 10:15:08 -0700

    Add setHint/getHint to PhutilLockException for recent lock changes
    Summary: Ref T13202. See PHI889. See D19702. A small part of that change affected libphutil, but got lost in the shuffle with all the Arcanist/wilds stuff in my local workspace.
    
    Test Plan: See D19702.
    
    Reviewers: amckinley
    
    Reviewed By: amckinley
    
    Maniphest Tasks: T13202
    
    Differential Revision: https://secure.phabricator.com/D19720

--- src/filesystem/PhutilLockException.php
+++ src/filesystem/PhutilLockException.php
@@ -1,3 +1,16 @@
 <?php
 
-final class PhutilLockException extends Exception {}
+final class PhutilLockException extends Exception {
+
+  private $hint;
+
+  public function setHint($hint) {
+    $this->hint = $hint;
+    return $this;
+  }
+
+  public function getHint() {
+    return $this->hint;
+  }
+
+}
