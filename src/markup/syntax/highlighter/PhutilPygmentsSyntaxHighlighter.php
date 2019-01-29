fc2d2d4065998233989a0886702c51695367246e
epriestley <git@epriestley.com>
2019-01-29 05:55:53 -0800

    Use "exec" to skip the Dash intermediate shell for "pygmentize"
    Summary:
    See T13224. A user reports the `setTimeout()` not actually killing Pygments under Ubuntu, where the default shell is "dash".
    
    I think what's happening here is:
    
      - Dash creates a strict subprocess always, Bash only does sometimes. If you "bash" a single simple command, it automatically behaves like you "exec"'d it and replaces itself.
      - So we end up with "dash" as our subprocess, and then "pygments" under it.
      - `setTimeout()` does SIGTERM + wait + SIGKILL.
      - When we SIGTERM, I'd guess it propagates to "pygments" but "pygments" ignores it (it's stuck inside regexp explosion land).
      - When we SIGKILL, I think it kills "dash" and leaves "pygments" alive.
    
    I didn't really test any of this since it's theoretically sound and adding "exec" is generally safe, and the reporting user says it fixed things.
    
    Test Plan: Highlighted some non-problematic code locally to make sure this didn't break anything in the working case.
    
    Reviewers: amckinley
    
    Reviewed By: amckinley
    
    Differential Revision: https://secure.phabricator.com/D20054

--- src/markup/syntax/highlighter/PhutilPygmentsSyntaxHighlighter.php
+++ src/markup/syntax/highlighter/PhutilPygmentsSyntaxHighlighter.php
@@ -21,8 +21,13 @@ final class PhutilPygmentsSyntaxHighlighter extends Phobject {
 
     if ($language) {
       $language = $this->getPygmentsLexerNameFromLanguageName($language);
+
+      // See T13224. Under Ubuntu, avoid leaving an intermedite "dash" shell
+      // process so we hit "pygmentize" directly if we have to SIGKILL this
+      // because it explodes.
+
       $future = new ExecFuture(
-        'pygmentize -O encoding=utf-8 -O stripnl=False -f html -l %s',
+        'exec pygmentize -O encoding=utf-8 -O stripnl=False -f html -l %s',
         $language);
 
       $scrub = false;
