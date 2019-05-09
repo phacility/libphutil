609b1532f5121507bc641c845ab8628e0b324d9d
epriestley <git@epriestley.com>
2019-05-09 12:34:49 -0700

    Provide an extension point for handling hyperlinks in remarkup in a special way
    Summary:
    Ref T5378. Although some dicussion there talks about separating "self-uri" matching and non-self-URI matching into two phases, the URI matching code has enough weird magic that it felt cleaner to put them in one phase with an extension point.
    
    Match URIs into structured data, then allow extensions to specialize the rendering behavior. This matches three flavors of URI:
    
      - "uri" can be specialized.
      - "<uri>" is a "literal" URI and can NOT be specialized. This already exists/works.
      - "{uri}" is an "embedded" URI and can ONLY be specialized (the default rendering is just the plain text).
    
    On its own, this change doesn't affect any rendering behavior.
    
    Test Plan:
    Added a small extension to Phabricator to recognize self-URIs, then built this very basic prototype of "handle self-URIs in a special way":
    
    {F6440351}
    
    Reviewers: amckinley, joshuaspence
    
    Reviewed By: joshuaspence
    
    Maniphest Tasks: T5378
    
    Differential Revision: https://secure.phabricator.com/D20511

--- src/markup/engine/remarkup/markuprule/PhutilRemarkupHyperlinkRule.php
+++ src/markup/engine/remarkup/markuprule/PhutilRemarkupHyperlinkRule.php
@@ -2,6 +2,8 @@
 
 final class PhutilRemarkupHyperlinkRule extends PhutilRemarkupRule {
 
+  const KEY_HYPERLINKS = 'hyperlinks';
+
   public function getPriority() {
     return 400.0;
   }
@@ -13,7 +15,13 @@ final class PhutilRemarkupHyperlinkRule extends PhutilRemarkupRule {
     // don't appear in normal text or normal URLs.
     $text = preg_replace_callback(
       '@<(\w{3,}://[^\s'.PhutilRemarkupBlockStorage::MAGIC_BYTE.']+?)>@',
-      array($this, 'markupHyperlink'),
+      array($this, 'markupHyperlinkAngle'),
+      $text);
+
+    // We match "{uri}", but do not link it by default.
+    $text = preg_replace_callback(
+      '@{(\w{3,}://[^\s'.PhutilRemarkupBlockStorage::MAGIC_BYTE.']+?)}@',
+      array($this, 'markupHyperlinkCurly'),
       $text);
 
     // Anything else we match "ungreedily", which means we'll look for
@@ -31,42 +39,49 @@ final class PhutilRemarkupHyperlinkRule extends PhutilRemarkupRule {
     return $text;
   }
 
-  protected function markupHyperlink(array $matches) {
+  public function markupHyperlinkAngle(array $matches) {
+    return $this->markupHyperlink('<', $matches);
+  }
+
+  public function markupHyperlinkCurly(array $matches) {
+    return $this->markupHyperlink('{', $matches);
+  }
+
+  protected function markupHyperlink($mode, array $matches) {
+    $raw_uri = $matches[1];
+
     try {
-      $uri = new PhutilURI($matches[1]);
+      $uri = new PhutilURI($raw_uri);
     } catch (Exception $ex) {
       return $matches[0];
     }
 
-    $protocol = $uri->getProtocol();
-
-    $protocols = $this->getEngine()->getConfig(
-      'uri.allowed-protocols',
-      array());
-
-    if (!idx($protocols, $protocol)) {
-      // If this URI doesn't use a whitelisted protocol, don't link it. This
-      // is primarily intended to prevent javascript:// silliness.
-      return $this->getEngine()->storeText($matches[1]);
-    }
-
-    return $this->storeRenderedHyperlink($matches[1]);
-  }
-
-  protected function storeRenderedHyperlink($link) {
-    return $this->getEngine()->storeText($this->renderHyperlink($link));
-  }
-
-  protected function renderHyperlink($link) {
     $engine = $this->getEngine();
 
-    if ($engine->isTextMode()) {
-      return $link;
+    $token = $engine->storeText($raw_uri);
+
+    $list_key = self::KEY_HYPERLINKS;
+    $link_list = $engine->getTextMetadata($list_key, array());
+
+    $link_list[] = array(
+      'token' => $token,
+      'uri' => $raw_uri,
+      'mode' => $mode,
+    );
+
+    $engine->setTextMetadata($list_key, $link_list);
+
+    return $token;
+  }
+
+  protected function renderHyperlink($link, $is_embed) {
+    // If the URI is "{uri}" and no handler picked it up, we just render it
+    // as plain text.
+    if ($is_embed) {
+      return $this->renderRawLink($link, $is_embed);
     }
 
-    if ($engine->getState('toc')) {
-      return $link;
-    }
+    $engine = $this->getEngine();
 
     $same_window = $engine->getConfig('uri.same-window', false);
     if ($same_window) {
@@ -84,6 +99,14 @@ final class PhutilRemarkupHyperlinkRule extends PhutilRemarkupRule {
         'rel' => 'noreferrer',
       ),
       $link);
+  }
+
+  private function renderRawLink($link, $is_embed) {
+    if ($is_embed) {
+      return '{'.$link.'}';
+    } else {
+      return $link;
+    }
   }
 
   protected function markupHyperlinkUngreedy($matches) {
@@ -116,7 +139,96 @@ final class PhutilRemarkupHyperlinkRule extends PhutilRemarkupRule {
       return $matches[0];
     }
 
-    return hsprintf('%s%s', $this->markupHyperlink(array(null, $match)), $tail);
+    $link = $this->markupHyperlink(null, array(null, $match));
+
+    return hsprintf('%s%s', $link, $tail);
+  }
+
+  public function didMarkupText() {
+    $engine = $this->getEngine();
+
+    $protocols = $engine->getConfig('uri.allowed-protocols', array());
+    $is_toc = $engine->getState('toc');
+    $is_text = $engine->isTextMode();
+    $is_mail = $engine->isHTMLMailMode();
+
+    $list_key = self::KEY_HYPERLINKS;
+    $raw_list = $engine->getTextMetadata($list_key, array());
+
+    $links = array();
+    foreach ($raw_list as $key => $link) {
+      $token = $link['token'];
+      $raw_uri = $link['uri'];
+      $mode = $link['mode'];
+
+      $is_embed = ($mode === '{');
+      $is_literal = ($mode === '<');
+
+      // If we're rendering in a "Table of Contents" or a plain text mode,
+      // we're going to render the raw URI without modifications.
+      if ($is_toc || $is_text) {
+        $result = $this->renderRawLink($raw_uri, $is_embed);
+        $engine->overwriteStoredText($token, $result);
+        continue;
+      }
+
+      // If this URI doesn't use a whitelisted protocol, don't link it. This
+      // is primarily intended to prevent "javascript://" silliness.
+      $uri = new PhutilURI($raw_uri);
+      $protocol = $uri->getProtocol();
+      $valid_protocol = idx($protocols, $protocol);
+      if (!$valid_protocol) {
+        $result = $this->renderRawLink($raw_uri, $is_embed);
+        $engine->overwriteStoredText($token, $result);
+        continue;
+      }
+
+      // If the URI is written as "<uri>", we'll render it literally even if
+      // some handler would otherwise deal with it.
+      // If we're rendering for HTML mail, we also render literally.
+      if ($is_literal || $is_mail) {
+        $result = $this->renderHyperlink($raw_uri, $is_embed);
+        $engine->overwriteStoredText($token, $result);
+        continue;
+      }
+
+      // Otherwise, this link is a valid resource which extensions are allowed
+      // to handle.
+      $links[$key] = $link;
+    }
+
+    if (!$links) {
+      return;
+    }
+
+    foreach ($links as $key => $link) {
+      $links[$key] = new PhutilRemarkupHyperlinkRef($link);
+    }
+
+    $extensions = PhutilRemarkupHyperlinkEngineExtension::getAllLinkEngines();
+    foreach ($extensions as $extension) {
+      $extension = id(clone $extension)
+        ->setEngine($engine)
+        ->processHyperlinks($links);
+
+      foreach ($links as $key => $link) {
+        $result = $link->getResult();
+        if ($result !== null) {
+          $engine->overwriteStoredText($link->getToken(), $result);
+          unset($links[$key]);
+        }
+      }
+
+      if (!$links) {
+        break;
+      }
+    }
+
+    // Render any remaining links in a normal way.
+    foreach ($links as $link) {
+      $result = $this->renderHyperlink($link->getURI(), $link->isEmbed());
+      $engine->overwriteStoredText($link->getToken(), $result);
+    }
   }
 
 }
